37373790b6bebd940e7f30f4a617bb760e63bd48
Jeffrey Persons <jperman8@gmail.com>
2020-05-14 12:37:06 -0700

    Character: Cleanup transitions for facing and posture states

--- Assets/Code/Controllers/PenguinController.cs
+++ Assets/Code/Controllers/PenguinController.cs
@@ -1,15 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 
 public class PenguinController : MonoBehaviour
 {
-    private MovementState state;
-    private enum MovementState { STANDING, LYING }
+    private Facing  facing;
+    private Posture posture;
+    private enum Facing  { LEFT,     RIGHT }
+    private enum Posture { STANDING, LYING }
 
     [SerializeField] private float walkingSpeed = default;
     [SerializeField] private float slidingSpeed = default;
 
-    private float amountToRotate;
     private float inputTolerance = 0.15f;
     [SerializeField] private string horizontalInputAxisName = default;
     [SerializeField] private string verticalInputAxisName   = default;
@@ -17,74 +19,109 @@ public class PenguinController : MonoBehaviour
     private Vector2 initialSpawnPosition;
     private Vector2 inputVelocity;
     private Rigidbody2D rigidBody;
+    private BoxCollider2D collider;
 
     public void Reset()
     {
-        state = MovementState.STANDING;
+        facing  = Facing.RIGHT;
+        posture = Posture.STANDING;
+
         inputVelocity      = Vector2.zero;
         rigidBody.velocity = inputVelocity;
         rigidBody.position = initialSpawnPosition;
     }
     void Awake()
     {
-        rigidBody            = gameObject.transform.GetComponent<Rigidbody2D>();
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        collider  = gameObject.GetComponent<BoxCollider2D>();
         initialSpawnPosition = rigidBody.position;
-        inputVelocity        = Vector2.zero;
-        rigidBody.velocity   = inputVelocity;
+        Reset();
     }
+
     void Update()
     {
-        float yInput = Input.GetAxisRaw(verticalInputAxisName);
-        if (yInput != 0)
+        float xInput = GetNormalizedInput(horizontalInputAxisName, inputTolerance);
+        if (xInput < 0 && facing == Facing.RIGHT)
         {
-            MovementState previousState = state;
-            state = (yInput < 0) ? MovementState.LYING : MovementState.STANDING;
-
-            amountToRotate = 0.0f;
-            if (state == MovementState.STANDING && previousState == MovementState.LYING)
-            {
-                amountToRotate = 90.0f;
-            }
-            if (state == MovementState.LYING && previousState == MovementState.STANDING)
-            {
-                amountToRotate = -90.0f;
-            }
+            facing = Facing.LEFT;
+            rigidBody.transform.localScale *= new Vector2(-1.00f, 1.00f);
+        }
+        else if (xInput > 0 && facing == Facing.LEFT)
+        {
+            facing = Facing.RIGHT;
+            rigidBody.transform.localScale *= new Vector2(-1.00f, 1.00f);
         }
 
-        float xInput = Input.GetAxisRaw(horizontalInputAxisName);
-        if (Mathf.Abs(xInput) >= inputTolerance)
+        float yInput = GetNormalizedInput(verticalInputAxisName, inputTolerance);
+        if (yInput < 0 && posture == Posture.STANDING)
         {
-            // only acceptable for right now...really bad...
-            // try: https://stackoverflow.com/questions/26568542/flipping-a-2d-sprite-animation-in-unity-2d in morning
-            rigidBody.transform.localScale = new Vector2(xInput < 0? -2.50f : 2.50f, 2.50f);
+            LieDown();
+        }
+        else if (yInput > 0 && posture == Posture.LYING)
+        {
+            StandUp();
         }
 
-        switch (state)
+        switch (posture)
         {
-            case MovementState.STANDING:
-                inputVelocity = new Vector2(walkingSpeed * xInput, 0);
-            break;
-            case MovementState.LYING:    inputVelocity = new Vector2(slidingSpeed * xInput, 0);  break;
-            default:                     inputVelocity = new Vector2(0, 0);  Debug.LogError($"State({state}) is not a valid enum"); break;
+            case Posture.STANDING: inputVelocity = new Vector2(walkingSpeed * xInput, 0);          break;
+            case Posture.LYING:    inputVelocity = new Vector2(slidingSpeed * xInput, 0);          break;
+            default:               inputVelocity = new Vector2(0, 0); LogInvalidEnumError(facing); break;
         }
     }
     void FixedUpdate()
     {
         rigidBody.velocity = inputVelocity;
 
-        /*
-        // really bad...try this instead in morning: https://forum.unity.com/threads/make-object-stand-up-after-thrown-on-the-floor.158142/
-        // probably need to offset y as well, too...(should probably learn about quarternions first and why euler angles dont work, too!)
-        Quaternion startRotation = transform.rotation;
-        Quaternion endRotation = new Quaternion (0.0F,transform.rotation.y,0.0F,transform.rotation.w);
-        transform.rotation = Quaternion.Slerp(startRotation, endRotation, Time.deltaTime*2.0F);
-        if (amountToRotate != 0.0f)
-        {
-            rigidBody.transform.Rotate(new Vector3(0, amountToRotate, 0));
-        }
-        */
         Quaternion startRotation = transform.rotation;
         Quaternion endRotation = new Quaternion(0.0f, transform.rotation.y, 0.0f, transform.rotation.w);
         transform.rotation = Quaternion.Slerp(startRotation, endRotation, Time.fixedDeltaTime * 2.0f);
+    }
+
+    void ChangePosture(Posture newPosture)
+    {
+        if (newPosture == posture)
+        {
+            return;
+        }
+        switch (newPosture)
+        {
+            case Posture.STANDING: StandUp(); break;
+            case Posture.LYING:    LieDown(); break;
+            default: LogInvalidEnumError(newPosture); break;
+        }
+    }
+
+    void FaceLeft()
+    {
+        facing = Facing.LEFT;
+        rigidBody.transform.localScale *= new Vector2(-1.00f, 1.00f);
+    }
+    void FaceRight()
+    {
+        facing = Facing.RIGHT;
+        rigidBody.transform.localScale *= new Vector2(-1.00f, 1.00f);
+    }
+    void LieDown()
+    {
+        posture = Posture.LYING;
+    }
+    void StandUp()
+    {
+        posture = Posture.STANDING;
+
+    }
+    void RotateTo()
+    {
+    }
+
+    private static float GetNormalizedInput(string name, float tolerance)
+    {
+        float input = Input.GetAxisRaw(name);
+        return Math.Abs(input) >= tolerance ? input : 0.00f;
+    }
+    private void LogInvalidEnumError(Enum enumValue)
+    {
+        Debug.LogError($"{nameof(enumValue)}({enumValue}) is not a valid enum");
     }
 }
