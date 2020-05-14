f974eb344a96990f496e6c68bf339eb00d2ca13d
Jeffrey Persons <jperman8@gmail.com>
2020-05-14 10:15:08 -0700

    Character: Add controls for standing and sliding states

--- Assets/Code/Controllers/PenguinController.cs
+++ Assets/Code/Controllers/PenguinController.cs
@@ -3,8 +3,16 @@
 
 public class PenguinController : MonoBehaviour
 {
-    [SerializeField] private float  penguinHorizontalSpeed   = default;
-    [SerializeField] private string inputAxisName            = default;
+    private MovementState state;
+    private enum MovementState { STANDING, LYING }
+
+    [SerializeField] private float walkingSpeed = default;
+    [SerializeField] private float slidingSpeed = default;
+
+    private float amountToRotate;
+    private float inputTolerance = 0.15f;
+    [SerializeField] private string horizontalInputAxisName = default;
+    [SerializeField] private string verticalInputAxisName   = default;
 
     private Vector2 initialSpawnPosition;
     private Vector2 inputVelocity;
@@ -12,7 +20,8 @@ public class PenguinController : MonoBehaviour
 
     public void Reset()
     {
-        inputVelocity       = Vector2.zero;
+        state = MovementState.STANDING;
+        inputVelocity      = Vector2.zero;
         rigidBody.velocity = inputVelocity;
         rigidBody.position = initialSpawnPosition;
     }
@@ -25,10 +34,57 @@ public class PenguinController : MonoBehaviour
     }
     void Update()
     {
-        inputVelocity = new Vector2(penguinHorizontalSpeed * Input.GetAxisRaw(inputAxisName), 0);
+        float yInput = Input.GetAxisRaw(verticalInputAxisName);
+        if (yInput != 0)
+        {
+            MovementState previousState = state;
+            state = (yInput < 0) ? MovementState.LYING : MovementState.STANDING;
+
+            amountToRotate = 0.0f;
+            if (state == MovementState.STANDING && previousState == MovementState.LYING)
+            {
+                amountToRotate = 90.0f;
+            }
+            if (state == MovementState.LYING && previousState == MovementState.STANDING)
+            {
+                amountToRotate = -90.0f;
+            }
+        }
+
+        float xInput = Input.GetAxisRaw(horizontalInputAxisName);
+        if (Mathf.Abs(xInput) >= inputTolerance)
+        {
+            // only acceptable for right now...really bad...
+            // try: https://stackoverflow.com/questions/26568542/flipping-a-2d-sprite-animation-in-unity-2d in morning
+            rigidBody.transform.localScale = new Vector2(xInput < 0? -2.50f : 2.50f, 2.50f);
+        }
+
+        switch (state)
+        {
+            case MovementState.STANDING:
+                inputVelocity = new Vector2(walkingSpeed * xInput, 0);
+            break;
+            case MovementState.LYING:    inputVelocity = new Vector2(slidingSpeed * xInput, 0);  break;
+            default:                     inputVelocity = new Vector2(0, 0);  Debug.LogError($"State({state}) is not a valid enum"); break;
+        }
     }
     void FixedUpdate()
     {
         rigidBody.velocity = inputVelocity;
+
+        /*
+        // really bad...try this instead in morning: https://forum.unity.com/threads/make-object-stand-up-after-thrown-on-the-floor.158142/
+        // probably need to offset y as well, too...(should probably learn about quarternions first and why euler angles dont work, too!)
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = new Quaternion (0.0F,transform.rotation.y,0.0F,transform.rotation.w);
+        transform.rotation = Quaternion.Slerp(startRotation, endRotation, Time.deltaTime*2.0F);
+        if (amountToRotate != 0.0f)
+        {
+            rigidBody.transform.Rotate(new Vector3(0, amountToRotate, 0));
+        }
+        */
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = new Quaternion(0.0f, transform.rotation.y, 0.0f, transform.rotation.w);
+        transform.rotation = Quaternion.Slerp(startRotation, endRotation, Time.fixedDeltaTime * 2.0f);
     }
 }
