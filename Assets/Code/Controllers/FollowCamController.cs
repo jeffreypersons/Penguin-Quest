ea0d7944645fc9566742a0415dfc80c32151c068
Jeff Persons <jperman8@gmail.com>
2020-05-20 17:02:17 -0700

    Game Scene: Improve environmental visuals and background (#23)
    Adds a ground object in the foreground, and mountains in background, with a light blue skybox, and camera controller/settings adjusted to keep just the right amount of the scene in view as the penguin moves around.
    
    
    Commit log
    * Assets: Add mountains svg asset
    * Project Settings: Add background/living-things tags/layers
    * Assets: Remove old placeholder sprites
    * Assets: Remove unused editor script
    * Camera: Force camera controller to always run*
    
    Rearranged game scene such that the camera viewport better captures the scene, and does so whether the script is running in game or edit mode.

--- Assets/Code/Controllers/FollowCamController.cs
+++ Assets/Code/Controllers/FollowCamController.cs
@@ -1,14 +1,31 @@
 ﻿using UnityEngine;
 
 
-// example offset: 6, 6, -10
+[ExecuteAlways]
 public class FollowCamController : MonoBehaviour
 {
-    public Transform player;
-    public Vector3 offset;
+    [Header("Game object to follow")]
+    [Tooltip("Subject for the camera to move in unison with")]
+    [SerializeField] private Transform subject;
 
+    [Header("Camera offset from subject")]
+    [Tooltip("ie offset(0, 10, 0) the subject will be centered 10 units above the subject")]
+    [SerializeField] private Vector3 offset;
+
+    private Vector3 PositionToFollow
+    {
+        get => new Vector3(subject.position.x, subject.position.y, 0);
+    }
+
+    void Awake()
+    {
+        if (!subject)
+        {
+            Debug.LogError($"Subject must be provided to camera script `{GetType().Name}` - no object assigned");
+        }
+    }
     void Update()
     {
-        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+        transform.position = PositionToFollow + offset;
     }
 }
