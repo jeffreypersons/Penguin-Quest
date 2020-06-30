4b17744ba1117e2f2d981321fe4499bb93c142fa
Jeffrey Persons <jperman8@gmail.com>
2020-06-30 15:41:00 -0700

    Add custom slider attributes

---
+++ Assets/Code/RatioSliderAttribute.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+// example usage:
+//     [TagSelector] [SerializeField] private string[] tagsOfButtonsToHideOnMenuOpen = new string[] { };
+//
+// note: see `Editor/CustomRatioSliderPropertyDrawer` for implementation
+public class RatioSliderAttribute : PropertyAttribute
+{
+    public float Min;
+    public float Max;
+    private float val;
+
+    public float Value { get => val; set => Mathf.Clamp(val, Min, Max); }
+
+    public RatioSliderAttribute(float min, float max)
+    {
+        Min = min;
+        Max = max;
+    }
+}
