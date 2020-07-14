23f961348d216d16b9eaeeb7cd846381423091c7
Jeff Persons <jperman8@gmail.com>
2020-07-13 22:14:49 -0700

    FollowCamera: Clamp offsets if they force the camera's subject outside of viewport bounds (#48)
    In particular, it clamps the offsets to prevent the penguin from fully leaving the screen, and enables toggling of active-following on and off.

--- Assets/Code/Controllers/FollowCameraController.cs
+++ Assets/Code/Controllers/FollowCameraController.cs
@@ -10,33 +10,34 @@ public class FollowCameraController : MonoBehaviour
     [Header("Game object to follow")]
     [Tooltip("Subject for the camera to move in unison with")]
     [SerializeField] private Transform subject;
+    private Renderer subjectRenderer;
 
-    [Header("Camera offset from subject")]
+    private const float MAX_FOLLOW_DISTANCE = 100;
+    [Header("Follow Settings")]
     [Tooltip("x offset from subject")]
-    [SerializeField] [Range(-30, 30)] private float xOffset = default;
+    [SerializeField] [Range(-MAX_FOLLOW_DISTANCE, MAX_FOLLOW_DISTANCE)] private float xOffset = default;
     [Tooltip("y offset from subject")]
-    [SerializeField] [Range(-30, 30)] private float yOffset = default;
-    float zOffset;
+    [SerializeField] [Range(-MAX_FOLLOW_DISTANCE, MAX_FOLLOW_DISTANCE)] private float yOffset = default;
+    [Tooltip("Prevent subject's sprite renderer from leaving the screen by clamping offsets")]
+    [SerializeField] private bool keepSubjectInScreen = true;
+    [Tooltip("Toggle for actively following")]
+    [SerializeField] private bool isActivelyFollowing = true;
 
-    private Vector3 Offset
+    private Vector3 screenSize;
+    private Vector3 screenMin;
+    private Vector3 screenMax;
+
+    private void Reset()
     {
-        get
-        {
-            return new Vector3(xOffset, yOffset, zOffset);
-        }
+        // forces screensize to update on first update pass
+        screenSize = Vector3.zero;
+        screenMin = Vector3.zero;
+        screenMax = Vector3.zero;
     }
-
-
-    private Vector3 PositionToFollow
-    {
-        get => new Vector3(subject.position.x, subject.position.y, 0);
-    }
-
     void Awake()
     {
         cam = gameObject.GetComponent<Camera>();
-        zOffset = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane)).z;
-
+        subjectRenderer = gameObject.GetComponent<Renderer>();
         if (!subject)
         {
             Debug.LogWarning($"No subject assigned to follow, `{GetType().Name}` - no object assigned");
@@ -44,9 +45,51 @@ public class FollowCameraController : MonoBehaviour
     }
     void LateUpdate()
     {
-        if (subject != null && subject.transform.hasChanged)
+        if (!isActivelyFollowing)
         {
-            transform.position = PositionToFollow + Offset;
+            return;
+        }
+
+        if (screenSize.x != Screen.width || screenSize.y != Screen.height)
+        {
+            screenSize = new Vector3(Screen.width, Screen.height, 0.00f);
+            screenMin = cam.ViewportToWorldPoint(new Vector3(0.00f, 0.00f, cam.nearClipPlane));
+            screenMax = cam.ViewportToWorldPoint(new Vector3(1.00f, 1.00f, cam.nearClipPlane));
+        }
+
+        if (subject != null)
+        {
+            cam.transform.position = ComputeCameraPosition();
+        }
+    }
+
+    // we don't worry about the case that the subject overlaps multiple sides,
+    // in other words, we assume its bounds are smaller than viewport
+    private Vector3 ComputeCameraPosition()
+    {
+        Vector3 subjectPosition;
+        Vector3 subjectHalfSize;
+        if (subjectRenderer == null)
+        {
+            subjectPosition = subject.position;
+            subjectHalfSize = Vector3.zero;
+        }
+        else
+        {
+            subjectPosition = subjectRenderer.bounds.center;
+            subjectHalfSize = subjectRenderer.bounds.extents;
+        }
+
+        if (keepSubjectInScreen)
+        {
+            return new Vector3(
+                Mathf.Clamp(subjectPosition.x + xOffset, screenMin.x - subjectHalfSize.x, screenMax.x - subjectHalfSize.x),
+                Mathf.Clamp(subjectPosition.y + yOffset, screenMin.y - subjectHalfSize.y, screenMax.y - subjectHalfSize.y),
+                screenMin.z);
+        }
+        else
+        {
+            return new Vector3(subjectPosition.x + xOffset, subjectPosition.y + yOffset, screenMin.z);
         }
     }
 }
