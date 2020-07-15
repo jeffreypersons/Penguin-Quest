a8a66fa3c5ea05c3741623bb6cf49876fdba3da5
Jeffrey Persons <jperman8@gmail.com>
2020-07-14 20:06:42 -0700

    Add smoothing/zoom/target tolerance filtering, and viewport meta data for an improved camera experience

--- Assets/Code/Controllers/FollowCameraController.cs
+++ Assets/Code/Controllers/FollowCameraController.cs
@@ -1,78 +1,104 @@
 ﻿using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 
 // todo: look into adding some sort of smoothing when the camera moves
 [ExecuteAlways]
 public class FollowCameraController : MonoBehaviour
 {
+    private const float FOV_DEFAULT    =   50.00f;
+    private const float FOV_MIN        =   10.00f;
+    private const float FOV_MAX        =  100.00f;
+    private const float OFFSET_DEFAULT =    0.00f;
+    private const float OFFSET_MIN     = -100.00f;
+    private const float OFFSET_MAX     =  100.00f;
+    private const float ZOOM_SPEED_DEFAULT =  10.00f;
+    private const float ZOOM_SPEED_MIN     =   0.01f;
+    private const float ZOOM_SPEED_MAX     =  50.00f;
+    private const float MOVE_SPEED_DEFAULT =  10.00f;
+    private const float MOVE_SPEED_MIN     =   0.01f;
+    private const float MOVE_SPEED_MAX     = 100.00f;
+    private const float TARGET_DISTANCE_TOLERANCE = 0.15f;
+
     private Camera cam;
+    private ViewportInfo viewport;
+    public enum FollowMode { MoveWithSubject, MoveAfterLeavingView };
 
-    [Header("Game object to follow")]
-    [Tooltip("Subject for the camera to move in unison with")]
-    [SerializeField] private Transform subject;
-    private Renderer subjectRenderer;
+    [Header("Subject to Follow")]
+    [Tooltip("Transform of subject for camera to follow (does not have to be 'visible')")]
+    [SerializeField]  private Transform subject;
+    [HideInInspector] private Renderer subjectRenderer;
 
-    private const float MAX_FOLLOW_DISTANCE = 100;
-    [Header("Follow Settings")]
-    [Tooltip("x offset from subject")]
-    [SerializeField] [Range(-MAX_FOLLOW_DISTANCE, MAX_FOLLOW_DISTANCE)] private float xOffset = default;
-    [Tooltip("y offset from subject")]
-    [SerializeField] [Range(-MAX_FOLLOW_DISTANCE, MAX_FOLLOW_DISTANCE)] private float yOffset = default;
-    [Tooltip("Prevent subject's sprite renderer from leaving the screen by clamping offsets")]
-    [SerializeField] private bool keepSubjectInScreen = true;
+    [Tooltip("Adjust move speed (how fast the camera follows the subject)")]
+    [Range(MOVE_SPEED_MIN, MOVE_SPEED_MAX)] [SerializeField] private float moveSpeed = MOVE_SPEED_DEFAULT;
+
+    [Header("Follow Position Relative to Subject")]
+    [Tooltip("x offset from subject (subject is on left of camera if positive, right if negative)")]
+    [Range(OFFSET_MIN, OFFSET_MAX)] [SerializeField] private float xOffset = OFFSET_DEFAULT;
+    [Tooltip("y offset from subject (subject is bellow camera if positive, above if negative)")]
+    [Range(OFFSET_MIN, OFFSET_MAX)] [SerializeField] private float yOffset = OFFSET_DEFAULT;
+
+    [Header("Follow Behavior")]
+    [Tooltip("Mode for camera movement relative to to subject")]
+    [SerializeField] private FollowMode followMode = FollowMode.MoveWithSubject;
+    [Tooltip("Clamp offsets to prevent subject's renderer from leaving camera viewport")]
+    [SerializeField] private bool keepSubjectInView = true;
     [Tooltip("Toggle for actively following")]
-    [SerializeField] private bool isActivelyFollowing = true;
+    [SerializeField] private bool isActivelyRunning = true;
 
-    private Vector3 screenSize;
-    private Vector3 screenMin;
-    private Vector3 screenMax;
+    [Header("Zoom Behavior")]
+    [Tooltip("Adjust field of view (how 'zoomed in' the camera is)")]
+    [Range(FOV_MIN, FOV_MAX)] [SerializeField] private float fieldOfView = FOV_DEFAULT;
+    [Tooltip("Adjust zoom speed (how fast the camera FOV is adjusted)")]
+    [Range(ZOOM_SPEED_MIN, ZOOM_SPEED_MAX)] [SerializeField] private float zoomSpeed = ZOOM_SPEED_DEFAULT;
 
-    private void Reset()
-    {
-        // forces screensize to update on first update pass
-        screenSize = Vector3.zero;
-        screenMin = Vector3.zero;
-        screenMax = Vector3.zero;
-    }
     void Awake()
     {
         cam = gameObject.GetComponent<Camera>();
+        Debug.Log("A" + cam);
         subjectRenderer = gameObject.GetComponent<Renderer>();
         if (!subject)
         {
-            Debug.LogWarning($"No subject assigned to follow, `{GetType().Name}` - no object assigned");
+            Debug.LogError($"No subject assigned to follow, `{GetType().Name}` - no object assigned");
         }
+    }
+    void Start()
+    {
+        viewport = new ViewportInfo(cam);
     }
     void LateUpdate()
     {
-        if (!isActivelyFollowing)
+        if (!isActivelyRunning)
         {
             return;
         }
-
-        if (screenSize.x != Screen.width || screenSize.y != Screen.height)
+        bool hasViewportChanged = viewport.SyncChanges();
+        bool hasSubjectChanged  = subject.transform.hasChanged;
+        bool isAtTargetPosition    = Mathf.Abs(fieldOfView - cam.transform.position.x) <= TARGET_DISTANCE_TOLERANCE;
+        bool isAtTargetFieldOfView = Mathf.Abs(fieldOfView - cam.fieldOfView) <= TARGET_DISTANCE_TOLERANCE;
+        if (hasViewportChanged || !isAtTargetFieldOfView)
         {
-            screenSize = new Vector3(Screen.width, Screen.height, 0.00f);
-            screenMin = cam.ViewportToWorldPoint(new Vector3(0.00f, 0.00f, cam.nearClipPlane));
-            screenMax = cam.ViewportToWorldPoint(new Vector3(1.00f, 1.00f, cam.nearClipPlane));
+            Debug.Log("Fov");
+            cam.fieldOfView = Mathf.MoveTowards(cam.fieldOfView, fieldOfView, Time.deltaTime * zoomSpeed);
         }
-
-        if (subject != null)
+        if (hasViewportChanged || !isAtTargetPosition)
         {
-            cam.transform.position = ComputeCameraPosition();
+            Vector2 target = Vector2.MoveTowards(cam.transform.position, ComputeTargetPosition(), Time.deltaTime * moveSpeed);
+            cam.transform.position = new Vector3(target.x, target.y, viewport.NearClipOffset);
+            Debug.Log(cam.transform.position);
         }
     }
 
     // we don't worry about the case that the subject overlaps multiple sides,
     // in other words, we assume its bounds are smaller than viewport
-    private Vector3 ComputeCameraPosition()
+    private Vector2 ComputeTargetPosition()
     {
-        Vector3 subjectPosition;
-        Vector3 subjectHalfSize;
+        Vector2 subjectPosition;
+        Vector2 subjectHalfSize;
         if (subjectRenderer == null)
         {
             subjectPosition = subject.position;
-            subjectHalfSize = Vector3.zero;
+            subjectHalfSize = Vector2.zero;
         }
         else
         {
@@ -80,16 +106,15 @@ public class FollowCameraController : MonoBehaviour
             subjectHalfSize = subjectRenderer.bounds.extents;
         }
 
-        if (keepSubjectInScreen)
+        if (keepSubjectInView)
         {
             return new Vector3(
-                Mathf.Clamp(subjectPosition.x + xOffset, screenMin.x - subjectHalfSize.x, screenMax.x - subjectHalfSize.x),
-                Mathf.Clamp(subjectPosition.y + yOffset, screenMin.y - subjectHalfSize.y, screenMax.y - subjectHalfSize.y),
-                screenMin.z);
+                Mathf.Clamp(subjectPosition.x + xOffset, viewport.Min.x - subjectHalfSize.x, viewport.Max.x - subjectHalfSize.x),
+                Mathf.Clamp(subjectPosition.y + yOffset, viewport.Min.y - subjectHalfSize.y, viewport.Max.y - subjectHalfSize.y));
         }
         else
         {
-            return new Vector3(subjectPosition.x + xOffset, subjectPosition.y + yOffset, screenMin.z);
+            return new Vector2(subjectPosition.x + xOffset, subjectPosition.y + yOffset);
         }
     }
 }
