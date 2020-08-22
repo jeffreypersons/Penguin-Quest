26d8b3d4e4c15f4c7175f20aff9947ddfdfee85c
Jeffrey Persons <jperman8@gmail.com>
2020-08-22 14:23:47 -0700

    Add utils test stub

---
+++ Assets/Code/Tests/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assets.Code.Tests
+{
+    /*
+    [Test]
+    public void Vector2_Perpendicular_Test()
+    {
+        var a = new Vector2(5, -10);
+        var b = a.PerpendicularClockwise();
+        var c = a.PerpendicularCounterClockwise();
+
+        Assert.AreEqual(new Vector2(-10, -5), b);
+        Assert.AreEqual(new Vector2(10, 5), c);
+    }
+    */
+}
