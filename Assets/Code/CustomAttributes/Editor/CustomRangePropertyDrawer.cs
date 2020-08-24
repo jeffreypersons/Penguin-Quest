e3219722dabb6a4ceed09964a4140f07a91eabbf
Jeffrey Persons <jperman8@gmail.com>
2020-08-24 16:27:15 -0700

    Add stubs for customrange attribute

---
+++ Assets/Code/CustomAttributes/Editor/CustomRangePropertyDrawer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using CustomAttributes;
+
+
+[CustomPropertyDrawer(typeof(CustomRangeAttribute))]
+public class CustomRangePropertyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+
+    }
+}
