4bb09143006f510daa9b6cc170df27f7aac4cf7b
Jeffrey Persons <jperman8@gmail.com>
2021-07-10 15:26:29 -0400

    Project Settings: Update Unity Version to latest 2021.1.14f1

--- Assets/Code/Data/GameEventCenter.cs
+++ Assets/Code/Data/GameEventCenter.cs
@@ -4,9 +4,9 @@
 // note events are triggered and handled ENTIRELY programmatically (via listeners and invocations)
 public static class GameEventCenter
 {
-    public static GameEvent<string>     enemyHit    = new GameEvent<string>();
-    public static GameEvent<int>        enemyKilled = new GameEvent<int>();
-    public static GameEvent<PlayerStatsInfo> scoreChange = new GameEvent<PlayerStatsInfo>();
+    public static GameEvent<string>           enemyHit    = new GameEvent<string>();
+    public static GameEvent<int>              enemyKilled = new GameEvent<int>();
+    public static GameEvent<PlayerStatsInfo>  scoreChange = new GameEvent<PlayerStatsInfo>();
 
     public static GameEvent<GameSettingsInfo> startNewGame = new GameEvent<GameSettingsInfo>();
     public static GameEvent<PlayerStatsInfo>  pauseGame    = new GameEvent<PlayerStatsInfo>();
