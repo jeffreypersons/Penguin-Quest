6aaee64d8a396dc41c5d381fbcda2b156b1a14cb
Jeffrey Persons <jperman8@gmail.com>
2022-02-23 09:49:04 -0500

    RaycasterSettings: Rename fields and properties to be consistent

--- Assets/PenguinQuest/Code/Data/RayCasterSettings.cs
+++ Assets/PenguinQuest/Code/Data/RayCasterSettings.cs
@@ -12,20 +12,20 @@ namespace PenguinQuest.Data
         [Header("Cast Settings")]
 
         [Tooltip("How much distance between each ray (if relevant)?")]
-        [SerializeField] [Range(0.00f, 100.00f)] private float  raySpacing  = 0.25f;
+        [SerializeField] [Range(0.10f, 100.00f)] private float  distanceBetweenRays = 0.25f;
 
         [Tooltip("What delta along the ray do we start the cast from?")]
-        [SerializeField] [Range(-10.00f, 10.00f)] private float rayOffset   = 0.25f;
+        [SerializeField] [Range(-10.00f, 10.00f)] private float rayCastOffset       = 0.25f;
 
         [Tooltip("At what max distance do we send a cast until?")]
-        [SerializeField] [Range(0.00f, 1000.00f)] private float maxDistance = 2.50f;
+        [SerializeField] [Range(0.00f, 1000.00f)] private float maxRayDistance      = 2.50f;
 
         [Tooltip("What layers do we take into consider when testing for a ray hit?")]
         [SerializeField] private LayerMask targetLayers = ~0;
 
-        public float     Offset       => rayOffset;
-        public float     MaxDistance  => maxDistance;
-        public LayerMask TargetLayers => targetLayers;
-        public float     RaySpacing   => raySpacing;
+        public float     Offset              => rayCastOffset;
+        public float     MaxDistance         => maxRayDistance;
+        public LayerMask TargetLayers        => targetLayers;
+        public float     DistanceBetweenRays => distanceBetweenRays;
     }
 }
