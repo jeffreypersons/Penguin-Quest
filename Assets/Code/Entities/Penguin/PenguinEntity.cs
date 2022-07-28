402662a712a095b3d418d3faa1f1203ff4e6b2a2
Jeffrey Persons <jperman8@gmail.com>
2022-07-28 08:55:48 -0400

    Move collider constraints into their own file

--- Assets/Code/Entities/Penguin/PenguinEntity.cs
+++ Assets/Code/Entities/Penguin/PenguinEntity.cs
@@ -4,19 +4,6 @@ using PenguinQuest.Extensions;
 
 namespace PenguinQuest.Controllers
 {
-    [System.Flags]
-    public enum PenguinColliderConstraints
-    {
-        None               = 0,
-        DisableHead        = 1 << 1,
-        DisableTorso       = 1 << 2,
-        DisableFlippers    = 1 << 3,
-        DisableFeet        = 1 << 4,
-        DisableBoundingBox = 1 << 5,
-        DisableAll         = ~0,
-    }
-
-
     /*
     Collection of penguin components.
 
