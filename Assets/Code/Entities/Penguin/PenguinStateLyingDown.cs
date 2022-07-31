30ee1d2cdce8519ab1abcb05219dc40ee7982ba1
Jeffrey Persons <jperman8@gmail.com>
2022-07-31 17:37:02 -0400

    Add stubs for lying down and standing up

---
+++ Assets/Code/Entities/Penguin/PenguinStateLyingDown.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using PQ.Common;
+
+
+namespace PQ.Entities.Penguin
+{
+    public class PenguinStateLyingDown : FsmState
+    {
+        private PenguinBlob _blob;
+
+        public PenguinStateLyingDown(string name, PenguinBlob blob) : base(name) { _blob = blob; }
+
+        public override void Enter()
+        {
+            _blob.CharacterController.Settings = _blob.OnBellySettings;
+        }
+
+        public override void Exit()
+        {
+
+        }
+
+        public override void FixedUpdate()
+        {
+
+        }
+
+        public override void LateUpdate()
+        {
+
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Code/Entities/Penguin/PenguinStateStandingUp.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using PQ.Common;
+
+
+namespace PQ.Entities.Penguin
+{
+    public class PenguinStateStandingUp : FsmState
+    {
+        private PenguinBlob _blob;
+
+        public PenguinStateStandingUp(string name, PenguinBlob blob) : base(name) { _blob = blob; }
+
+        public override void Enter()
+        {
+            _blob.CharacterController.Settings = _blob.OnBellySettings;
+        }
+
+        public override void Exit()
+        {
+
+        }
+
+        public override void FixedUpdate()
+        {
+
+        }
+
+        public override void LateUpdate()
+        {
+
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+}
