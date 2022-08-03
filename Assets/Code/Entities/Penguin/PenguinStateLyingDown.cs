e84d3683d1fcad481dfb19effc971e45d2df73cf
Jeffrey Persons <jperman8@gmail.com>
2022-08-03 08:25:25 -0400

    First pass on replicating LieDownHandler logic into LieDownState

--- Assets/Code/Entities/Penguin/PenguinStateLyingDown.cs
+++ Assets/Code/Entities/Penguin/PenguinStateLyingDown.cs
@@ -12,27 +12,62 @@ namespace PQ.Entities.Penguin
 
         public override void Enter()
         {
-            //_blob.CharacterController.Settings = _blob.OnBellySettings;
+            _blob.Animation.TriggerParamLieDownParameter();
+
+            _blob.Animation.LieDownStarted  += OnLieDownAnimationStarted;
+            _blob.Animation.LieDownMidpoint += OnLieDownAnimationMidpoint;
+            _blob.Animation.LieDownEnded    += OnLieDownAnimationFinished;
         }
 
         public override void Exit()
         {
-
+            _blob.Animation.LieDownStarted  -= OnLieDownAnimationStarted;
+            _blob.Animation.LieDownMidpoint -= OnLieDownAnimationMidpoint;
+            _blob.Animation.LieDownEnded    -= OnLieDownAnimationFinished;
         }
 
-        public override void FixedUpdate()
-        {
 
+        private void OnLieDownAnimationStarted()
+        {
+            _blob.Animation.SetParamIsUpright(false);
+
+            // disable our box and feet, to prevent catching on edges when changing posture from OnFeet to OnBelly
+            _blob.ColliderConstraints =
+                PenguinColliderConstraints.DisableBoundingBox |
+                PenguinColliderConstraints.DisableFeet;
         }
 
-        public override void LateUpdate()
+        private void OnLieDownAnimationMidpoint()
         {
-
+            // disable our box and feet, to prevent catching on edges when changing posture from OnFeet to OnBelly
+            _blob.ColliderConstraints =
+                PenguinColliderConstraints.DisableBoundingBox |
+                PenguinColliderConstraints.DisableFeet        |
+                PenguinColliderConstraints.DisableFlippers;
         }
 
-        public override void Update()
+        private void OnLieDownAnimationFinished()
         {
+            // keep our feet and flippers disabled to avoid interference with ground while OnBelly,
+            // but enable everything else including bounding box
+            _blob.ColliderConstraints =
+                 PenguinColliderConstraints.DisableFeet |
+                 PenguinColliderConstraints.DisableFlippers;
 
+            _blob.CharacterController.Settings = _blob.OnBellySettings;
+            _blob.ReadjustBoundingBox(
+                offset:     new Vector2( 0,  5),
+                size:       new Vector2(25, 10),
+                edgeRadius: 1.25f
+            );
+
+            // todo: find a good way of having data for sliding and for onFeet that can be passed in here,
+            //       and those values can be adjusted, perhaps in their own scriptable objects?
+            //
+            // todo: configure bounding box for onbelly mode, and enable the collider back here,
+            //       after disabling in animation start, and then update in the following way...
+            //       penguinBlob.ColliderBoundingBox.bounds such that offset(x=0, y=5), size(x=25, y=10), edge-radius(1.25)
+            //
         }
     }
 }
