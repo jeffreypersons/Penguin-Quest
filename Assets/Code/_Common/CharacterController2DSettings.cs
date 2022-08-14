9f094129ed29b702130177b70a1a4c1317d8c48c
Jeffrey Persons <jperman8@gmail.com>
2022-08-14 11:00:28 -0400

    Standardize naming convention for setting fields and resync with editor settings

--- Assets/Code/_Common/CharacterController2DSettings.cs
+++ Assets/Code/_Common/CharacterController2DSettings.cs
@@ -13,17 +13,17 @@ namespace PQ.Common
         public float JumpStrength                                 => _jumpStrength;
         public float JumpAngle                                    => _jumpAngle;
 
-        public float MaxAscendableSlopeAngle                      => maxAscendableSlopeAngle;
-        public float MaxToleratedDistanceFromGround               => maxToleratedDistanceFromGround;
+        public float HorizontalMovementPeakSpeed                  => _horizontalMovementPeakSpeed;
+        public float MaxAscendableSlopeAngle                      => _maxAscendableSlopeAngle;
+        public float MaxToleratedDistanceFromGround               => _maxToleratedDistanceFromGround;
 
-        public bool  MaintainPerpendicularityToSurface            => maintainPerpendicularityToSurface;
-        public float SurfaceAlignmentRotationalStrength           => surfaceAlignmentRotationalStrength;
-        public float DegreesFromSurfaceNormalThreshold            => degreesFromSurfaceNormalThreshold;
+        public bool  MaintainPerpendicularityToSurface            => _maintainPerpendicularityToSurface;
+        public float SurfaceAlignmentRotationalStrength           => _surfaceAlignmentRotationalStrength;
+        public float DegreesFromSurfaceNormalThreshold            => _degreesFromSurfaceNormalThreshold;
 
-        public bool  EnableAutomaticAxisLockingForSmallVelocities => enableAutomaticAxisLockingForSmallVelocities;
-        public float LinearVelocityThreshold                      => linearVelocityThreshold;
-        public float AngularVelocityThreshold                     => angularVelocityThreshold;
-        
+        public bool  EnableAutomaticAxisLockingForSmallVelocities => _enableAutomaticAxisLockingForSmallVelocities;
+        public float LinearVelocityThreshold                      => _linearVelocityThreshold;
+        public float AngularVelocityThreshold                     => _angularVelocityThreshold;
 
 
         [Header("Animation Settings")]
@@ -31,12 +31,16 @@ namespace PQ.Common
                  "(ie 0.05 for blended delayed transition taking at least 20 frames, 1 for instant transition)")]
         [Range(0.01f, 1.00f)] [SerializeField] private float _locomotionBlendStep = 0.10f;
 
+        [Header("Walk Settings")]
+        [Tooltip("Step size used to move the character")]
+        [Range(1, 50)][SerializeField] private float _horizontalMovementPeakSpeed = 5.00f;
+
         // todo: once we fully switch over to kinematic physics, then just give a height, distance to apex
         [Header("Jump Settings")]
         [Tooltip("Strength of jump force in newtons")]
         [Range(25000, 250000)] [SerializeField] private float _jumpStrength = 50000;
 
-        [Tooltip("Angle to jump (in degrees counterclockwise to the penguin's forward facing direction)")]
+        [Tooltip("Angle to jump (in degrees counterclockwise to the character's forward facing direction)")]
         [Range(0, 90)] [SerializeField] private float _jumpAngle = 45f;
 
 
@@ -46,34 +50,34 @@ namespace PQ.Common
 
         [Header("Surface Handling Ranges")]
 
-        [Tooltip("At what slope angle do we allow the penguin to walk up to?")]
-        [SerializeField] [Range(0.00f, 70.00f)] private float maxAscendableSlopeAngle = 45.00f;
+        [Tooltip("At what slope angle do we allow the character to walk up to?")]
+        [SerializeField] [Range(0.00f, 70.00f)] private float _maxAscendableSlopeAngle = 45.00f;
 
         [Tooltip("At what distance from ground do we consider the character no longer grounded?")]
-        [SerializeField] [Range(0.25f, 25.00f)] private float maxToleratedDistanceFromGround = 0.30f;
+        [SerializeField] [Range(0.25f, 25.00f)] private float _maxToleratedDistanceFromGround = 0.30f;
 
 
         [Header("Surface Alignment Sensitives (Tolerances for Jitter Reduction)")]
 
         [Tooltip("Should we rotate to align perpendicular to the surface normal?")]
-        [SerializeField] private bool maintainPerpendicularityToSurface = true;
+        [SerializeField] private bool _maintainPerpendicularityToSurface = true;
 
         [Tooltip("Rigidity of alignment with surface normal (ie 0 for max softness, 1 for no kinematic softness)")]
-        [SerializeField] [Range(0.00f, 1.00f)] private float surfaceAlignmentRotationalStrength = 0.10f;
+        [SerializeField] [Range(0.00f, 1.00f)] private float _surfaceAlignmentRotationalStrength = 0.10f;
 
         [Tooltip("At what degrees between up axis and surface normal is considered to be misaligned?")]
-        [SerializeField] [Range(0.01f, 20.00f)] private float degreesFromSurfaceNormalThreshold = 0.01f;
+        [SerializeField] [Range(0.01f, 20.00f)] private float _degreesFromSurfaceNormalThreshold = 0.01f;
 
 
         [Header("Movement Sensitives (Tolerances for Jitter Reduction)")]
 
         [Tooltip("Should we automatically lock movement axes when velocities are within thresholds?")]
-        [SerializeField] private bool enableAutomaticAxisLockingForSmallVelocities = true;
+        [SerializeField] private bool _enableAutomaticAxisLockingForSmallVelocities = true;
 
         [Tooltip("What linear velocities (in x or y units) do we consider to be small enough to ignore?")]
-        [SerializeField] [Range(0.01f, 20.00f)] private float linearVelocityThreshold = 5.00f;
+        [SerializeField] [Range(0.01f, 20.00f)] private float _linearVelocityThreshold = 5.00f;
 
         [Tooltip("What angular velocities (in degrees) do we consider to be small enough to ignore?")]
-        [SerializeField] [Range(0.01f, 20.00f)] private float angularVelocityThreshold = 5.00f;
+        [SerializeField] [Range(0.01f, 20.00f)] private float _angularVelocityThreshold = 5.00f;
     }
 }
