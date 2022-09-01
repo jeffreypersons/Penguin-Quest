d629054e4867afa39e4a46b6149e04fc1c653816
Jeffrey Persons <jperman8@gmail.com>
2022-09-01 08:51:09 -0400

    Rename again to RayHit

--- Assets/Code/_Common/RayHit.cs
+++ Assets/Code/_Common/RayHit.cs
@@ -3,14 +3,14 @@
 
 namespace PQ.Common
 {
-    public struct CasterHit
+    public struct RayHit
     {
         public readonly Vector2    point;
         public readonly Vector2    normal;
         public readonly float      distance;
         public readonly Collider2D collider;
 
-        public CasterHit(Vector2 point, Vector2 normal, float distance, Collider2D collider)
+        public RayHit(Vector2 point, Vector2 normal, float distance, Collider2D collider)
         {
             this.point    = point;
             this.normal   = normal;
