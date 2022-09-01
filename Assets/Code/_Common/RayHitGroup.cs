b9501f86b8fb27632ba0420aaad094f16fd2ea4d
Jeffrey Persons <jperman8@gmail.com>
2022-09-01 09:29:29 -0400

    Extract out a RayHitGroup primitive for storing results of multiple simultaneous raycasts

--- Assets/Code/_Common/RayHitGroup.cs
+++ Assets/Code/_Common/RayHitGroup.cs
@@ -3,15 +3,15 @@
 
 namespace PQ.Common
 {
-    /* Result info of a single ray-surface intersection. */
-    public struct RayHit
+    /* Results of multiple ray intersections. */
+    public struct RayHitGroup
     {
         public readonly Vector2    point;
         public readonly Vector2    normal;
         public readonly float      distance;
         public readonly Collider2D collider;
 
-        public RayHit(Vector2 point, Vector2 normal, float distance, Collider2D collider)
+        public RayHitGroup(Vector2 point, Vector2 normal, float distance, Collider2D collider)
         {
             this.point    = point;
             this.normal   = normal;
