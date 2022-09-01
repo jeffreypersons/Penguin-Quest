94109dba95ab1f5924bf78e04363a0eb2f3959d1
Jeffrey Persons <jperman8@gmail.com>
2022-09-01 08:59:09 -0400

    Refactor RayCaster.Hit into a seperate file as RayHit (#116)
    * Move cast hit out into its own file
    * Rename Hit to RayHit to differentiate it from built in castHit types

---
+++ Assets/Code/_Common/RayHit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+namespace PQ.Common
+{
+    public struct RayHit
+    {
+        public readonly Vector2    point;
+        public readonly Vector2    normal;
+        public readonly float      distance;
+        public readonly Collider2D collider;
+
+        public RayHit(Vector2 point, Vector2 normal, float distance, Collider2D collider)
+        {
+            this.point    = point;
+            this.normal   = normal;
+            this.distance = distance;
+            this.collider = collider;
+        }
+    }
+}
