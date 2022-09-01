fde72ab1515fb1b861fa2f34e42d3e0ba99759d5
Jeffrey Persons <jperman8@gmail.com>
2022-09-01 08:50:11 -0400

    Rename CastHit to CasterHit to differentiate it from built in castHit types

--- Assets/Code/_Common/CasterHit.cs
+++ Assets/Code/_Common/CasterHit.cs
@@ -3,14 +3,14 @@
 
 namespace PQ.Common
 {
-    public struct CastHit
+    public struct CasterHit
     {
         public readonly Vector2    point;
         public readonly Vector2    normal;
         public readonly float      distance;
         public readonly Collider2D collider;
 
-        public CastHit(Vector2 point, Vector2 normal, float distance, Collider2D collider)
+        public CasterHit(Vector2 point, Vector2 normal, float distance, Collider2D collider)
         {
             this.point    = point;
             this.normal   = normal;
--- Assets/Code/_Common/RayCaster.cs
+++ Assets/Code/_Common/RayCaster.cs
@@ -15,26 +15,26 @@ namespace PQ.Common
 
 
         /* Shoot out a line between given points, seeing if a TargetLayer is hit. */
-        public CastHit? CastBetween(Vector2 from, Vector2 to)
+        public CasterHit? CastBetween(Vector2 from, Vector2 to)
         {
             return Cast(from, (to - from).normalized, MaxDistance, LayerMask);
         }
 
         /* Shoot out a line from point to max distance from that point until a TargetLayer is hit. */
-        public CastHit? CastFromPoint(Vector2 point, Vector2 direction)
+        public CasterHit? CastFromPoint(Vector2 point, Vector2 direction)
         {
             return Cast(point, direction, MaxDistance, LayerMask);
         }
 
         /* Shoot out a line from edge of collider to distance from that point until a TargetLayer is hit. */
-        public CastHit? CastFromCollider(Collider2D collider, Vector2 direction)
+        public CasterHit? CastFromCollider(Collider2D collider, Vector2 direction)
         {
             Vector2 point = FindPositionOnColliderEdgeInGivenDirection(collider, direction);
             return Cast(point, direction, MaxDistance, LayerMask);
         }
 
 
-        private static CastHit? Cast(Vector2 origin, Vector2 direction, float distance, LayerMask layerMask)
+        private static CasterHit? Cast(Vector2 origin, Vector2 direction, float distance, LayerMask layerMask)
         {
             RaycastHit2D castHit2D = Physics2D.Raycast(origin, direction, distance, layerMask);
             if (!castHit2D)
@@ -42,7 +42,7 @@ namespace PQ.Common
                 return null;
             }
 
-            return new CastHit(
+            return new CasterHit(
                 point:    castHit2D.point,
                 normal:   castHit2D.normal,
                 distance: castHit2D.distance,
