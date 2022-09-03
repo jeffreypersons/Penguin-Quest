6586f494f0681a2375f76b8876e77da4ebfd6e3c
Jeffrey Persons <jperman8@gmail.com>
2022-09-03 14:15:22 -0400

    Add ability to draw cast in editor for debugging

--- Assets/Code/_Common/RayCaster.cs
+++ Assets/Code/_Common/RayCaster.cs
@@ -8,8 +8,9 @@ namespace PQ.Common
     */
     public class RayCaster
     {
-        public float     MaxDistance { get; set; } = Mathf.Infinity;
-        public LayerMask LayerMask   { get; set; } = ~0;
+        public float     MaxDistance      { get; set; } = Mathf.Infinity;
+        public LayerMask LayerMask        { get; set; } = ~0;
+        public bool      DrawCastInEditor { get; set; } = true;
 
         public RayCaster() { }
 
@@ -34,9 +35,15 @@ namespace PQ.Common
         }
 
 
-        private static RayHit? Cast(Vector2 origin, Vector2 direction, float distance, LayerMask layerMask)
+        private RayHit? Cast(Vector2 origin, Vector2 direction, float distance, LayerMask layerMask)
         {
             RaycastHit2D castHit2D = Physics2D.Raycast(origin, direction, distance, layerMask);
+
+            #if UNITY_EDITOR
+            if (DrawCastInEditor)
+                DrawCastResultAsLineInEditor(origin, direction, distance, castHit2D);
+            #endif
+
             if (!castHit2D)
             {
                 return null;
@@ -50,11 +57,32 @@ namespace PQ.Common
             );
         }
 
+
         private static Vector2 FindPositionOnColliderEdgeInGivenDirection(Collider2D collider, Vector2 direction)
         {
             Vector2 center = collider.bounds.center;
             collider.bounds.IntersectRay(new Ray(center, direction), out float distanceFromCenterToEdge);
             return center - (distanceFromCenterToEdge * direction);
         }
+        
+        #if UNITY_EDITOR
+        private static void DrawCastResultAsLineInEditor(Vector2 origin, Vector2 direction, float distance, RaycastHit2D hit)
+        {
+            Color color;
+            Vector2 terminal;
+            if (hit)
+            {
+                color = Color.green;
+                terminal = hit.point;
+            }
+            else
+            {
+                color = Color.red;
+                terminal = origin + distance * direction;
+            }
+
+            Debug.DrawLine(origin, terminal, color, duration: Time.deltaTime);
+        }
+        #endif
     }
 }
