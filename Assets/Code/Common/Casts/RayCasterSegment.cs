510ae3a685aaa62a8f6bdf4b9e9e13db44ecb064
Jeffrey Persons <jperman8@gmail.com>
2022-10-10 07:13:08 -0400

    Remove min/max ray count

--- Assets/Code/Common/Casts/RayCasterSegment.cs
+++ Assets/Code/Common/Casts/RayCasterSegment.cs
@@ -14,9 +14,6 @@ namespace PQ.Common.Casts
         private Vector2 _rayDirection;
         private RayCaster _rayCaster;
 
-        public const int MinRayCount = 3;
-        public const int MaxRayCount = 1000;
-
         public Vector2   SegmentStart     => _segmentStart;
         public Vector2   SegmentMid       => Vector2.Lerp(_segmentStart, _segmentEnd, 0.50f);
         public Vector2   SegmentEnd       => _segmentEnd;
@@ -33,10 +30,10 @@ namespace PQ.Common.Casts
 
 
         public RayCasterSegment() :
-            this(segmentStart: Vector2.zero, segmentEnd: Vector2.zero, rayCount: MinRayCount, Vector2.right)
+            this(segmentStart: Vector2.zero, segmentEnd: Vector2.zero, Vector2.right)
         { }
 
-        public RayCasterSegment(Vector2 segmentStart, Vector2 segmentEnd, int rayCount, Vector2 rayDirection)
+        public RayCasterSegment(Vector2 segmentStart, Vector2 segmentEnd, Vector2 rayDirection)
         {
             _rayCaster = new RayCaster { MaxDistance = Mathf.Infinity, LayerMask = ~0 };
             UpdatePositioning(segmentStart, segmentEnd, rayDirection);
