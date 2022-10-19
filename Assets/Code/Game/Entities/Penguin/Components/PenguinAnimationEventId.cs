7a26763910f2d799580c6a112a935cdca07daaff
Jeffrey Persons <jperman8@gmail.com>
2022-10-19 07:22:39 -0400

    Remove accidently duplicated enum

--- Assets/Code/Game/Entities/Penguin/Components/PenguinAnimationEventId.cs
+++ Assets/Code/Game/Entities/Penguin/Components/PenguinAnimationEventId.cs
@@ -4,7 +4,6 @@ namespace PQ.Game.Entities.Penguin
 {
     // rather than hold a bunch of properties, we use these IDs for looking up which events to trigger (from animator),
     // or listen to (from client code), greatly consolidating animation boilerplate
-    // 
     public enum PenguinAnimationEventId
     {
         JumpLiftOff,
@@ -16,23 +15,5 @@ namespace PQ.Game.Entities.Penguin
         Fired,
         Used,
         FrontFootDown,
-    }
-
-    /*
-    Reminder: These parameters _must_ match the names in mecanim.
-    Unfortunately there is no easy way to generate the parameter names,
-    so just be careful to make sure that they match the parameters listed in the Unity Animator.
-    */
-    // todo: look into validation of the param names with the animator using below enums
-    public enum Params
-    {
-        Locomotion,
-        SlopeIntensity,
-        IsGrounded,
-        TriggerLieDown,
-        TriggerStandUp,
-        TriggerJumpUp,
-        TriggerFire,
-        TriggerUse,
     }
 }
