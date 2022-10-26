153cb99bf01aa444dac170913bad926dd85b8bac
Jeffrey Persons <jperman8@gmail.com>
2022-10-26 09:34:22 -0400

    RayHit: Rename range check for consistency

--- Assets/Code/Common/Casts/RayHit.cs
+++ Assets/Code/Common/Casts/RayHit.cs
@@ -1,24 +1,24 @@
-﻿using UnityEngine;
+﻿using System.Diagnostics.Contracts;
+using UnityEngine;
 
 
 namespace PQ.Common.Casts
 {
-    /* Result info of a single ray-surface intersection. */
+    /* Result info of a single ray-surface intersection, with extra convenience methods over built in cast result struct. */
     public struct RayHit
     {
         public readonly Vector2    point;
         public readonly Vector2    normal;
         public readonly float      distance;
         public readonly Collider2D collider;
-        
-        public bool HitWithin(float tolerance) => collider != null && distance <= tolerance;
 
         public override string ToString() =>
             $"{GetType().Name}:{{" +
                 $"point:{point}," +
                 $"normal:{normal}," +
                 $"distance:{distance}," +
-                $"collider:{collider}}}";
+                $"collider:{collider?.ToString() ?? "<None>"}}}";
+
 
         public RayHit(Vector2 point, Vector2 normal, float distance, Collider2D collider)
         {
@@ -28,6 +28,11 @@ namespace PQ.Common.Casts
             this.collider = collider;
         }
 
+        [Pure]
+        public bool HitInRange(float min, float max)
+        {
+            return collider != null && distance >= min && distance <= max;
+        }
 
         public static implicit operator bool(RayHit hit)
         {
