5f38f78e359e5897a6a7cecb301787f2387a5712
Jeffrey Persons <jperman8@gmail.com>
2022-11-03 09:13:05 -0400

    Remove misleading helper method

--- Assets/Sandbox/Minimal/GameplayInput.cs
+++ Assets/Sandbox/Minimal/GameplayInput.cs
@@ -16,8 +16,6 @@ namespace PQ.TestScenes.Minimal
         private static readonly Key[] downButtons  = { Key.DownArrow,  Key.S };
         private static readonly Key[] upButtons    = { Key.UpArrow,    Key.W };
 
-        private bool WasPressedThisFrame(Key key) => Keyboard.current[key].isPressed;
-
         public GameplayInput()
         {
             _axis = Vector2.zero;
@@ -40,8 +38,8 @@ namespace PQ.TestScenes.Minimal
         // Return the scalar value (-1, 1) of the winning key(s), if any, otherwise 0
         private float ReadAxis(Key[] negativeKeys, Key[] positiveKeys)
         {
-            var negativePressedCount = negativeKeys.Count(WasPressedThisFrame);
-            var positivePressedCount = positiveKeys.Count(WasPressedThisFrame);
+            var negativePressedCount = negativeKeys.Count(key => Keyboard.current[key].isPressed);
+            var positivePressedCount = positiveKeys.Count(key => Keyboard.current[key].isPressed);
             return negativePressedCount.CompareTo(positivePressedCount);
         }
     }
