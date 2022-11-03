7e93c21137651015c95edb3704f62ae8321c567c
Jeffrey Persons <jperman8@gmail.com>
2022-11-03 09:12:01 -0400

    Log input axes when changed

--- Assets/Sandbox/Minimal/GameplayInput.cs
+++ Assets/Sandbox/Minimal/GameplayInput.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 
@@ -6,27 +7,34 @@ namespace PQ.TestScenes.Minimal
 {
     public sealed class GameplayInput
     {
-        public float Horizontal { get; private set; }
-        public float Vertical   { get; private set; }
+        private Vector2 _axis;
+        public float Horizontal => _axis.x;
+        public float Vertical   => _axis.y;
 
         private static readonly Key[] leftButtons  = { Key.LeftArrow,  Key.A };
         private static readonly Key[] rightButtons = { Key.RightArrow, Key.D };
         private static readonly Key[] downButtons  = { Key.DownArrow,  Key.S };
         private static readonly Key[] upButtons    = { Key.UpArrow,    Key.W };
 
-        private bool WasPressedThisFrame(Key key) => Keyboard.current[key].wasPressedThisFrame;
+        private bool WasPressedThisFrame(Key key) => Keyboard.current[key].isPressed;
 
         public GameplayInput()
         {
-            Horizontal = 0f;
-            Vertical   = 0f;
+            _axis = Vector2.zero;
         }
 
 
         public void ReadInput()
         {
-            Horizontal = ReadAxis(negativeKeys: leftButtons, positiveKeys: rightButtons);
-            Vertical   = ReadAxis(negativeKeys: downButtons, positiveKeys: upButtons);
+            Vector2 axis = new(
+                x: ReadAxis(negativeKeys: leftButtons, positiveKeys: rightButtons),
+                y: ReadAxis(negativeKeys: downButtons, positiveKeys: upButtons)
+            );
+            if (_axis != axis)
+            {
+                _axis = axis;
+                Debug.Log($"input-axis changed from ({axis}) to ({_axis})");
+            }
         }
 
         // Return the scalar value (-1, 1) of the winning key(s), if any, otherwise 0
