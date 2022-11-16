fe150950eaad6f3067362d8655cdb5eda10e7270
Jeffrey Persons <jperman8@gmail.com>
2022-11-16 12:01:53 -0500

    IF DEF out custom editor drawers

--- Assets/Code/Game/EditorTools/CustomAttributes/Editor/ReadOnlyFieldPropertyDrawer.cs
+++ Assets/Code/Game/EditorTools/CustomAttributes/Editor/ReadOnlyFieldPropertyDrawer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using CustomAttributes;
 
@@ -19,3 +20,4 @@ public class ReadOnlyFieldPropertyDrawer : PropertyDrawer
         }
     }
 }
+#endif
--- Assets/Code/Game/EditorTools/CustomAttributes/Editor/TagSelectorPropertyDrawer.cs
+++ Assets/Code/Game/EditorTools/CustomAttributes/Editor/TagSelectorPropertyDrawer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using System;
 using CustomAttributes;
@@ -53,3 +54,4 @@ public class TagSelectorPropertyDrawer : PropertyDrawer
         }
     }
 }
+#endif
