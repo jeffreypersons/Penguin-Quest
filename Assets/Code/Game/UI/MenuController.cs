cfb2935d4cfd00dec9aa99235d0370cfc94f16d9
Jeffrey Persons <jperman8@gmail.com>
2022-11-20 20:27:01 -0500

    Replace all old UI prefabs with stubs

--- Assets/Code/Game/UI/MenuController.cs
+++ Assets/Code/Game/UI/MenuController.cs
@@ -3,37 +3,19 @@
 
 namespace PQ.Game.UI
 {
-    // todo: when incorporating this stuff directly in game state machine, get rid of the events,
-    //       and instead expose open/close directly
     public class MenuController : MonoBehaviour
     {
-        private GameEventCenter _eventCenter;
-
-
         void Awake()
-        {
-            _eventCenter = GameEventCenter.Instance;
-        }
-
-        void OnEnable()
-        {
-            _eventCenter.pauseGame .AddHandler(Open);
-            _eventCenter.resumeGame.AddHandler(Close);
-        }
-
-        void OnDisable()
-        {
-            _eventCenter.pauseGame .RemoveHandler(Open);
-            _eventCenter.resumeGame.RemoveHandler(Close);
-        }
-
-
-        private void Open()
         {
             // TBD
         }
 
-        private void Close()
+        public void Open()
+        {
+            // TBD
+        }
+
+        public void Close()
         {
             // TBD
         }
---
+++ Assets/Code/Game/UI/SplashScreenController.cs
@@ -0,0 +1,37 @@
+﻿using PQ.Game.Entities;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace PQ.Game.UI
+{
+    // todo: when incorporating this stuff directly in game state machine, get rid of the events,
+    //       and instead expose update stamina/etc directly
+    public class SplashScreenController : MonoBehaviour
+    {
+        [Header("Hud Elements")]
+        [SerializeField] private TMPro.TextMeshProUGUI _title;
+        [SerializeField] private TMPro.TextMeshProUGUI _subtitle;
+        [SerializeField] private TMPro.TextMeshProUGUI _prompt;
+
+        void Awake()
+        {
+            // TBD
+        }
+
+        void OnEnable()
+        {
+            // TBD
+        }
+
+        void OnDisable()
+        {
+            // TBD
+        }
+
+        private void HandleAnyButtonPressed()
+        {
+            // TBD - load scene?
+        }
+    }
+}
