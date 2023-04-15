0fc0ef474658c1b43c15fbdb29009348f53430aa
Jeffrey Persons <jperman8@gmail.com>
2023-04-15 08:55:16 -0400

    CameraController: Cleanup logging and validation

--- Assets/Code/Game/Camera/CameraController.cs
+++ Assets/Code/Game/Camera/CameraController.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Linq;
-using System.Text;
 using UnityEngine;
 using Cinemachine;
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 
 
 namespace PQ.Game.Camera
@@ -29,20 +27,21 @@ namespace PQ.Game.Camera
         [Tooltip("Configured Cinemachine virtual cameras")]
         [SerializeField] private CinemachineVirtualCamera[] _virtualCameras = Array.Empty<CinemachineVirtualCamera>();
 
-        public override string ToString()
-        {
-            // todo: add camera stats like viewport, etc
-            return $"{GetType()}(gameObject:{base.name})" +
-                $"\n  follow: {_followTarget?.name ?? "<none>"}";
-        }
 
         private string ExtractTargetName(Transform target) =>
-            (target == null || string.IsNullOrEmpty(target.name)) ? null : target.name;
+            (target == null || string.IsNullOrEmpty(target.name)) ? "<none>" : $"'{target.name}'";
 
         private bool IsFollowingTarget(Transform target) =>
             ReferenceEquals(_followTarget, target) &&
             _virtualCameras.All(vCam => ReferenceEquals(vCam.Follow, target));
 
+
+        public override string ToString()
+        {
+            // todo: add camera stats like viewport, etc
+            return $"{GetType()}(gameObject:{base.name})" +
+                $"\n  follow: {ExtractTargetName(_followTarget)}";
+        }
 
         public Transform FollowTarget
         {
@@ -57,9 +56,10 @@ namespace PQ.Game.Camera
                     return;
                 }
 
-                Debug.LogFormat($"Camera.FollowTarget set to '{0}' (previously '{1}')",
-                    ExtractTargetName(_followTarget) ?? "<none>",
-                    ExtractTargetName(value)         ?? "<none>");
+                Debug.LogFormat("Camera.FollowTarget set to {0} (previously {1})",
+                    $"{nameof(CameraController)}.{nameof(FollowTarget)}",
+                    ExtractTargetName(_followTarget),
+                    ExtractTargetName(value));
 
                 _followTarget = value;
                 for (int i = 0; i < _virtualCameras.Length; i++)
@@ -69,11 +69,35 @@ namespace PQ.Game.Camera
             }
         }
 
+
         void Awake()
+        {
+            EnsureComponentsNotMissing();
+
+            Debug.LogFormat("{0} attached to {1} is awake, with virtual cameras: {2}",
+                nameof(CameraController),
+                base.name,
+                $"[{string.Join(',', _virtualCameras.Select(vCam => vCam.name))}]");
+        }
+
+        #if UNITY_EDITOR
+        void OnValidate()
+        {
+            // note: only need to enforce components are not missing _if_ changed in inspector
+            if (!UnityEditor.EditorApplication.isPlaying)
+            {
+                return;
+            }
+            EnsureComponentsNotMissing();
+        }
+        #endif
+
+
+        private void EnsureComponentsNotMissing()
         {
             if (_mainCamera == null)
             {
-                throw new MissingComponentException($"Missing component for {base.name} - Main camera unassigned");
+                throw new MissingComponentException($"Missing component for {base.name} - main camera unassigned");
             }
             if (_brain == null)
             {
@@ -83,14 +107,6 @@ namespace PQ.Game.Camera
             {
                 throw new MissingComponentException($"Missing component(s) for {base.name} - virtual camera(s) unassigned");
             }
-
-            StringBuilder message = new();
-            for (int i = 0; i < _virtualCameras.Length; i++)
-            {
-                _virtualCameras[i].Follow = _followTarget;
-                message.Append($",{_virtualCameras[i].name}");
-            }
-            Debug.Log($"Camera attached to {base.name} is awake, with virtual cameras: [{0}]" + message.ToString());
         }
     }
 }
