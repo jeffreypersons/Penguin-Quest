8fa9550b83ac54c9ee85dae4c062af7d108518ce
Jeffrey Persons <jperman8@gmail.com>
2023-06-14 10:04:16 -0400

    Overhaul overlap scene 2 to only execute next step on command

--- Assets/_Experimental/Sandbox_Movement/Overlap_002/Body.cs
+++ Assets/_Experimental/Sandbox_Movement/Overlap_002/Body.cs
@@ -6,15 +6,12 @@ namespace PQ._Experimental.Overlap_002
 {
     public class Body : MonoBehaviour
     {
-        [SerializeField] private Rigidbody2D   _rigidbody;
-        [SerializeField] private BoxCollider2D _boxCollider;
+        private const int _preallocatedHitBufferSize = 16;
 
-        [SerializeField] private LayerMask _layerMask = default;
-        [SerializeField] [Range(0,   1)] private float _skinWidth = 0.075f;
-        [SerializeField] [Range(1, 100)] private int _preallocatedHitBufferSize = 16;
-
-        private ContactFilter2D _contactFilter;
+        private Rigidbody2D     _rigidbody;
+        private BoxCollider2D   _boxCollider;
         private RaycastHit2D[]  _hitBuffer;
+        private ContactFilter2D _contactFilter;
 
         public override string ToString() =>
             $"Mover{{" +
@@ -25,14 +22,12 @@ namespace PQ._Experimental.Overlap_002
                 $"AABB: bounds(center:{Bounds.center}, extents:{Bounds.extents})," +
             $"}}";
 
-        public Vector2 Position  => _rigidbody.position;
-        public float   Depth     => _rigidbody.transform.position.z;
-        public Bounds  Bounds    => _boxCollider.bounds;
-        public Vector2 Forward   => _rigidbody.transform.right.normalized;
-        public Vector2 Up        => _rigidbody.transform.up.normalized;
-        public float   SkinWidth => _skinWidth;
-        public Vector2 Extents   => _boxCollider.bounds.extents + new Vector3(_boxCollider.edgeRadius, _boxCollider.edgeRadius, 0f);
-
+        public Vector2 Position => _rigidbody.position;
+        public float   Depth    => _rigidbody.transform.position.z;
+        public Bounds  Bounds   => _boxCollider.bounds;
+        public Vector2 Forward  => _rigidbody.transform.right.normalized;
+        public Vector2 Up       => _rigidbody.transform.up.normalized;
+        public Vector2 Extents  => _boxCollider.bounds.extents;
 
         void Awake()
         {
@@ -48,9 +43,6 @@ namespace PQ._Experimental.Overlap_002
             _contactFilter = new ContactFilter2D();
             _hitBuffer     = new RaycastHit2D[_preallocatedHitBufferSize];
 
-            _boxCollider.edgeRadius = _skinWidth;
-            _boxCollider.size = new Vector2(1f - _skinWidth, 1f - _skinWidth);
-
             _rigidbody.interpolation = RigidbodyInterpolation2D.Interpolate;
             _rigidbody.isKinematic = true;
             _rigidbody.simulated   = true;
@@ -59,7 +51,7 @@ namespace PQ._Experimental.Overlap_002
 
             _contactFilter.useTriggers    = false;
             _contactFilter.useNormalAngle = false;
-            _contactFilter.SetLayerMask(_layerMask);
+            _contactFilter.SetLayerMask(LayerMask.GetMask("Solids"));
         }
 
         /* Immediately move body to given point. */
@@ -82,63 +74,16 @@ namespace PQ._Experimental.Overlap_002
         }
 
 
-        public bool CastAABB(Vector2 direction, float distance, out ReadOnlySpan<RaycastHit2D> hits, bool includeAlreadyOverlappingColliders)
+        public bool CastAABB(Vector2 direction, float distance, out RaycastHit2D hit)
         {
-            bool previousQueriesStartInColliders = Physics2D.queriesStartInColliders;
-            Physics2D.queriesStartInColliders = includeAlreadyOverlappingColliders;
-
-            int hitCount = _boxCollider.Cast(direction, _contactFilter, _hitBuffer, distance, ignoreSiblingColliders: true);
-            hits = _hitBuffer.AsSpan(0, hitCount);
-
-            Physics2D.queriesStartInColliders = previousQueriesStartInColliders;
-
-            return !hits.IsEmpty;
-        }
-
-        /*
-        Project a point along given direction until specific given collider is hit.
-
-        Note that in 3D we have collider.RayCast for this, but in 2D we have no built in way of checking a
-        specific collider (collider2D.RayCast confusingly casts _from_ it instead of _at_ it).
-        */
-        public bool CastRayAt(Collider2D collider, Vector2 origin, Vector2 direction, float distance, out RaycastHit2D hit, bool includeAlreadyOverlappingColliders)
-        {
-            int layer = collider.gameObject.layer;
-            bool queriesStartInColliders = Physics2D.queriesStartInColliders;
-            LayerMask includeLayers = _contactFilter.layerMask;
-            collider.gameObject.layer = Physics2D.IgnoreRaycastLayer;
-            Physics2D.queriesStartInColliders = includeAlreadyOverlappingColliders;
-            _contactFilter.SetLayerMask(~collider.gameObject.layer);
-
-            int hitCount = Physics2D.Raycast(origin, direction, _contactFilter, _hitBuffer, distance);
-
-            collider.gameObject.layer = layer;
-            _contactFilter.SetLayerMask(includeLayers);
-            Physics2D.queriesStartInColliders = queriesStartInColliders;
-
             hit = default;
-            for (int i = 0; i < hitCount; i++)
+            if (_boxCollider.Cast(direction, _contactFilter, _hitBuffer, distance) > 0)
             {
-                if (_hitBuffer[i].collider == collider)
-                {
-                    hit = _hitBuffer[i];
-                    break;
-                }
+                hit = _hitBuffer[0];
             }
             return hit;
         }
-        
 
-        /* Compute distance from center to edge of our bounding box in given direction. */
-        public float ComputeDistanceToEdge(Vector2 direction)
-        {
-            Bounds bounds = _boxCollider.bounds;
-            bounds.IntersectRay(new Ray(bounds.center, direction), out float distanceFromCenterToEdge);
-
-            // discard sign since distance is negative if starts within bounds (contrary to other ray methods)
-            return Mathf.Abs(distanceFromCenterToEdge);
-        }
-        
         /*
         Compute vector representing overlap amount between body and given collider, if any.
 
@@ -159,40 +104,5 @@ namespace PQ._Experimental.Overlap_002
             }
             return minimumSeparation;
         }
-
-
-        #if UNITY_EDITOR
-        void OnValidate()
-        {
-            _boxCollider.edgeRadius = _skinWidth;
-            _boxCollider.size = new Vector2(1f - _skinWidth, 1f - _skinWidth);
-            if (!Application.IsPlaying(this))
-            {
-                return;
-            }
-
-            if (_hitBuffer == null || _preallocatedHitBufferSize != _hitBuffer.Length)
-            {
-                _hitBuffer = new RaycastHit2D[_preallocatedHitBufferSize];
-            }
-        }
-
-        void OnDrawGizmos()
-        {
-            // draw a bounding box that should be identical to the BoxCollider2D bounds in the editor window,
-            // surrounded by an outer bounding box offset by our skin with, with a pair of arrows from the that
-            // should be identical to the transform's axes in the editor window
-            Bounds box = Bounds;
-            Vector2 center    = new(box.center.x, box.center.y);
-            Vector2 skinRatio = new(1f + (_skinWidth / box.extents.x), 1f + (_skinWidth / box.extents.y));
-            Vector2 xAxis     = box.extents.x * Forward;
-            Vector2 yAxis     = box.extents.y * Up;
-
-            GizmoExtensions.DrawRect(center, xAxis, yAxis, Color.black);
-            GizmoExtensions.DrawRect(center, skinRatio.x * xAxis, skinRatio.y * yAxis, Color.black);
-            GizmoExtensions.DrawArrow(from: center, to: center + xAxis, color: Color.red);
-            GizmoExtensions.DrawArrow(from: center, to: center + yAxis, color: Color.green);
-        }
-        #endif
     }
 }
