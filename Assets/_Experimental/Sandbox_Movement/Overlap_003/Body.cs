f62c385b779e773f9fb26cf90f1567aa2202c7d4
Jeffrey Persons <jperman8@gmail.com>
2023-06-17 10:31:22 -0400

    Duplicate overlap scene but this time rework to use capsules

---
+++ Assets/_Experimental/Sandbox_Movement/Overlap_003/Body.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+
+
+namespace PQ._Experimental.Overlap_003
+{
+    public class Body : MonoBehaviour
+    {
+        private const int _preallocatedHitBufferSize = 16;
+
+        private Rigidbody2D       _rigidbody;
+        private CapsuleCollider2D _capsuleCollider;
+        private RaycastHit2D[]    _hitBuffer;
+        private ContactFilter2D   _contactFilter;
+
+        public override string ToString() =>
+            $"Mover{{" +
+                $"Position:{Position}," +
+                $"Depth:{Depth}," +
+                $"Forward:{Forward}," +
+                $"Up:{Up}," +
+                $"AABB: bounds(center:{Bounds.center}, extents:{Bounds.extents})," +
+            $"}}";
+
+        public Vector2 Position => _rigidbody.position;
+        public float   Depth    => _rigidbody.transform.position.z;
+        public Bounds  Bounds   => _capsuleCollider.bounds;
+        public Vector2 Forward  => _rigidbody.transform.right.normalized;
+        public Vector2 Up       => _rigidbody.transform.up.normalized;
+        public Vector2 Extents  => _capsuleCollider.bounds.extents;
+
+
+        void Awake()
+        {
+            if (!transform.TryGetComponent<Rigidbody2D>(out var rigidbody))
+            {
+                throw new MissingComponentException($"Expected attached rigidbody2D - not found on {transform}");
+            }
+            if (!transform.TryGetComponent<CapsuleCollider2D>(out var capsuleCollider))
+            {
+                throw new MissingComponentException($"Expected attached collider2D - not found on {transform}");
+            }
+
+            _rigidbody       = rigidbody;
+            _capsuleCollider = capsuleCollider;
+
+            _contactFilter = new ContactFilter2D();
+            _hitBuffer     = new RaycastHit2D[_preallocatedHitBufferSize];
+
+            _rigidbody.interpolation = RigidbodyInterpolation2D.Interpolate;
+            _rigidbody.isKinematic = true;
+            _rigidbody.simulated   = true;
+            _rigidbody.useFullKinematicContacts = true;
+            _rigidbody.constraints = RigidbodyConstraints2D.None;
+
+            _contactFilter.useTriggers    = false;
+            _contactFilter.useNormalAngle = false;
+            _contactFilter.SetLayerMask(LayerMask.GetMask("Solids"));
+        }
+
+        /* Immediately move body to given point. */
+        public void MoveTo(Vector2 position)
+        {
+            _rigidbody.position = position;
+        }
+
+        /* Immediately move body by given amount. */
+        public void MoveBy(Vector2 delta)
+        {
+            _rigidbody.position += delta;
+        }
+
+        /* Interpolated move this amount. */
+        public void MovePosition(Vector2 startPositionThisFrame, Vector2 targetPositionThisFrame)
+        {
+            _rigidbody.position = startPositionThisFrame;
+            _rigidbody.MovePosition(targetPositionThisFrame);
+        }
+
+
+        public bool CastAABB(Vector2 direction, float distance, out RaycastHit2D hit)
+        {
+            hit = default;
+            if (_capsuleCollider.Cast(direction, _contactFilter, _hitBuffer, distance) > 0)
+            {
+                hit = _hitBuffer[0];
+            }
+            return hit;
+        }
+
+        /*
+        Compute vector representing overlap amount between body and given collider, if any.
+
+        Note that uses separating axis theorem to determine overlap, so may require more invocations to resolve overlap
+        for complex collider shapes (eg convex polygons).
+        */
+        public ColliderDistance2D ComputeMinimumSeparation(Collider2D collider)
+        {
+            if (collider == null)
+            {
+                return default;
+            }
+            ColliderDistance2D minimumSeparation = _capsuleCollider.Distance(collider);
+            return minimumSeparation.isValid ? minimumSeparation : default;
+        }
+    }
+}
---
+++ Assets/_Experimental/Sandbox_Movement/Overlap_003/GizmoExtensions.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+namespace PQ._Experimental.Overlap_003
+{
+    /*
+    Various utilities for drawing and manipulating gizmos.
+
+    Includes resetting of color to what it was previously before the call to minimize side effects.
+    */
+    public static class GizmoExtensions
+    {
+        private static readonly Color DefaultColor = Color.white;
+
+        /* Draw text at given world position. */
+        public static void DrawText(Vector2 position, string text, Color? color = null)
+        {
+            // since handles are only available in editor, this becomes a no op when running elsewhere
+            #if UNITY_EDITOR
+            Color previousColor = Handles.color;
+            Handles.color = color.GetValueOrDefault(DefaultColor);
+
+            Handles.Label(position, text);
+
+            Handles.color = previousColor;
+            #endif
+        }
+
+        /* Draw line between given world positions. */
+        public static void DrawLine((Vector2, Vector2) endpoints, Color? color = null)
+        {
+            Color previousColor = Gizmos.color;
+            Gizmos.color = color.GetValueOrDefault(DefaultColor);
+
+            Gizmos.DrawLine(endpoints.Item1, endpoints.Item2);
+
+            Gizmos.color = previousColor;
+        }
+
+        /* Draw sides of rectangle corresponding to the area represented by given relative origin and axes. */
+        public static void DrawRect(Vector2 origin, Vector2 xAxis, Vector2 yAxis, Color? color = null)
+        {
+            Color previousColor = Gizmos.color;
+            Gizmos.color = color.GetValueOrDefault(DefaultColor);
+
+            Vector2 min = origin - xAxis - yAxis;
+            Vector2 max = origin + xAxis + yAxis;
+            Vector2 leftBottom  = new(min.x, min.y);
+            Vector2 leftTop     = new(min.x, max.y);
+            Vector2 rightBottom = new(max.x, min.y);
+            Vector2 rightTop    = new(max.x, max.y);
+
+            Gizmos.DrawLine(leftTop,     rightTop);
+            Gizmos.DrawLine(leftBottom,  rightBottom);
+            Gizmos.DrawLine(leftBottom,  leftTop);
+            Gizmos.DrawLine(rightBottom, rightTop);
+
+            Gizmos.color = previousColor;
+        }
+        
+        /* Draw lines between given points. */
+        public static void DrawWaypoints(Vector2[] points, Color? color = null)
+        {
+            if (points == null || points.Length < 2)
+            {
+                return;
+            }
+
+            Color previousColor = Gizmos.color;
+            Gizmos.color = color.GetValueOrDefault(DefaultColor);
+
+            for (int i = 1; i < points.Length; i++)
+            {
+                Gizmos.DrawLine(points[i-1], points[i]);
+            }
+
+            Gizmos.color = previousColor;
+        }
+
+        /* Draw a 3d sphere at given world position. */
+        public static void DrawSphere(Vector2 origin, float radius, Color? color = null)
+        {
+            Color previousColor = Gizmos.color;
+            Gizmos.color = color.GetValueOrDefault(DefaultColor);
+
+            Gizmos.DrawSphere(origin, radius);
+
+            Gizmos.color = previousColor;
+        }
+
+        /*
+        Assumes arrow head length is nonzero and from,to are nonequal.
+        
+        Note that arrow head length and height are configured to be the same length for simplicity,
+        and sized relative to length of line.
+        */
+        public static void DrawArrow(Vector2 from, Vector2 to, Color? color = null,
+            float arrowheadSizeRatio = 0.10f)
+        {
+            Color previousColor = Gizmos.color;
+            Gizmos.color = color.GetValueOrDefault(DefaultColor);
+
+            Vector2 vector    = to - from;
+            Vector2 dir       = vector.normalized;
+            float length      = vector.magnitude;
+            float arrowLength = arrowheadSizeRatio * length;
+            Vector2 arrowheadBottom      = from + ((length - arrowLength) * dir);
+            Vector2 arrowheadBaseExtents = new Vector2(-dir.y, dir.x) * arrowLength * 0.50f;
+
+            Gizmos.DrawLine(from, to);
+            Gizmos.DrawLine(arrowheadBottom + arrowheadBaseExtents, to);
+            Gizmos.DrawLine(arrowheadBottom - arrowheadBaseExtents, to);
+
+            Gizmos.color = previousColor;
+        }
+    }
+}
