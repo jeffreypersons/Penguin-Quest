32548ab15ffa66ecf0eb3de8c896106a6156edf1
Jeffrey Persons <jperman8@gmail.com>
2023-06-22 09:14:17 -0400

    [Physics] Flesh out move test scene 3, replacing circles with boxes

--- Assets/_Experimental/Sandbox_Movement/Movement_003/Internal/KinematicLinearSolver2D.cs
+++ Assets/_Experimental/Sandbox_Movement/Movement_003/Internal/KinematicLinearSolver2D.cs
@@ -24,37 +24,13 @@ namespace PQ._Experimental.Movement_003.Internal
             float distance = delta.magnitude;
             Vector2 direction = delta.normalized;
 
-            if (!_body.CastCircle(direction, distance, out RaycastHit2D obstruction, true))
+            if (!_body.CastAABB(direction, distance, out RaycastHit2D obstruction))
             {
                 _body.MoveBy(delta);
                 return;
             }
 
             _body.MoveBy(obstruction.fraction * delta);
-        }
-
-
-        private void SnapToCollider(Collider2D collider)
-        {
-            Vector2 startPosition = _body.Position;
-            for (int i = 0; i < _maxMinSeparationSolves; i++)
-            {
-                ColliderDistance2D minSeparation = _body.ComputeMinimumSeparation(collider);
-                Vector2 offset = minSeparation.distance * minSeparation.normal;
-                if (offset == Vector2.zero)
-                {
-                    break;
-                }
-                _body.MoveBy(offset);
-            }
-            Vector2 endPosition = _body.Position;
-
-            if (startPosition != endPosition)
-            {
-                Vector2 markerExtents = 0.075f * Vector2.Perpendicular(endPosition - startPosition);
-                Debug.DrawLine(startPosition - markerExtents, startPosition + markerExtents, Color.red, 10f);
-                Debug.DrawLine(startPosition, endPosition, Color.white, 10f);
-            }
         }
     }
 }
