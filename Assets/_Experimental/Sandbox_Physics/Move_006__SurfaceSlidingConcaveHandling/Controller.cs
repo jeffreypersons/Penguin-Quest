94691b26ff13bac17fae707fb6785d42ed872a94
Jeffrey Persons <jperman8@gmail.com>
2023-07-26 09:22:58 -0400

    [Physics] Test scenes - Create initial 6 and 7 test scenes, rename 5/6/7 for clarity

---
+++ Assets/_Experimental/Sandbox_Physics/Move_006__SurfaceSlidingConcaveHandling/Controller.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+namespace PQ._Experimental.Physics.Move_006
+{
+    public class Controller : MonoBehaviour
+    {
+        [Range(0,  10)][SerializeField] private float _timeScale = 1f;
+        [Range(0, 100)][SerializeField] private float _moveSpeed = 5f;
+
+        private Vector2 _inputAxis;
+
+        private KinematicBody2D         _kinematicBody;
+        private KinematicLinearSolver2D _kinematicSolver;
+        private CircularBuffer<Vector2> _positionHistory;
+
+
+        void Awake()
+        {
+            Application.targetFrameRate = 60;
+
+            _kinematicBody   = new KinematicBody2D(transform);
+            _kinematicSolver = new KinematicLinearSolver2D(_kinematicBody);
+            _positionHistory = new CircularBuffer<Vector2>(capacity: 50);
+        }
+
+        void Update()
+        {
+            if (!Mathf.Approximately(Time.timeScale, _timeScale))
+            {
+                Time.timeScale = _timeScale;
+            }
+            _inputAxis = new Vector2(
+                x: (Keyboard.current[Key.A].isPressed ? -1f : 0f) + (Keyboard.current[Key.D].isPressed ? 1f : 0f),
+                y: (Keyboard.current[Key.S].isPressed ? -1f : 0f) + (Keyboard.current[Key.W].isPressed ? 1f : 0f)
+            );
+        }
+
+        void FixedUpdate()
+        {
+            Vector2 position = _kinematicBody.Position;
+            if (_positionHistory.IsEmpty || _positionHistory.Back != position)
+            {
+                _positionHistory.PushBack(_kinematicBody.Position);
+            }
+
+            if (!Mathf.Approximately(_inputAxis.x, 0f))
+            {
+                _kinematicSolver.Flip(horizontal: _inputAxis.x < 0, vertical: false);
+            }
+
+            Vector2 deltaPosition = Time.fixedDeltaTime * _moveSpeed * _inputAxis;
+            _kinematicSolver.Move(deltaPosition);
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            _kinematicSolver.RemoveOverlap(collision.collider);
+        }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            _kinematicSolver.RemoveOverlap(collision.collider);
+        }
+
+        void OnCollisionStay2D(Collision2D collision)
+        {
+            _kinematicSolver.RemoveOverlap(collision.collider);
+        }
+
+        void OnDrawGizmos()
+        {
+            if (!Application.IsPlaying(this) || _positionHistory.Size < 2)
+            {
+                return;
+            }
+
+            for (int i = 1; i < _positionHistory.Size; i++)
+            {
+                GizmoExtensions.DrawArrow(_positionHistory[i - 1], _positionHistory[i], Color.cyan);
+            }
+        }
+    }
+}
---
+++ Assets/_Experimental/Sandbox_Physics/Move_007__SurfaceSlidingGravity/KinematicLinearSolver2D.cs
@@ -0,0 +1,148 @@
+using System;
+using UnityEngine;
+
+
+namespace PQ._Experimental.Physics.Move_007
+{
+    /*
+    Collide and slide solver for movement.
+
+    Notes
+    - When moving along surfaces, we maintain a slight offset from the normal, such that contacts are
+      intentionally avoided. This way, we avoid getting caught on edges and corners as easily      
+    */
+    internal sealed class KinematicLinearSolver2D
+    {
+        private KinematicBody2D _body;
+
+        /* Number of iterations used to reach movement target before giving up. */
+        private const int MaxMoveIterations = 10;
+
+        /* Number of iterations used to reach no overlap before giving up. */
+        private const int MaxOverlapIterations = 5;
+
+        /* Amount which we consider to be (close enough to) zero. */
+        private const float Epsilon = 0.005f;
+
+        /* Amount used to ensure we don't get _too_ close to surfaces, to avoid getting stuck when moving tangential to a surface. */
+        private const float ContactOffset = 0.05f;
+
+
+        public KinematicLinearSolver2D(KinematicBody2D kinematicBody2D)
+        {
+            if (kinematicBody2D == null)
+            {
+                throw new ArgumentNullException($"Expected non-null {nameof(KinematicLinearSolver2D)}");
+            }
+            _body = kinematicBody2D;
+        }
+
+        public void Flip(bool horizontal, bool vertical)
+        {
+            Vector3 rotation = new Vector3(
+                x: vertical   ? 180f : 0f,
+                y: horizontal ? 180f : 0f,
+                z: 0f);
+
+            if (_body.Rotation != rotation)
+            {
+                _body.Rotation = rotation;
+            }
+        }
+
+        /*
+        Note that with edge colliders, the collider will end up on either side, as there is no 'internal area'.
+
+        This means that if our body starts in more overlapped position than separated from an edge collider, it will
+        resolve to the 'inside' of the edge.
+        
+        In practice, this is not an issue except when spawning, as any movement in the solver caps changes in position be no
+        greater than the body extents.
+        */
+        public void RemoveOverlap(Collider2D collider)
+        {
+            if (_body.IsFilteringLayerMask(collider.gameObject))
+            {
+                return;
+            }
+            
+            // note that we remove separation if ever so slightly above surface as well
+            Vector2 startPosition = _body.Position;
+            int iteration = MaxOverlapIterations;
+            ColliderDistance2D separation = _body.ComputeMinimumSeparation(collider);
+            while (iteration-- > 0 && separation.distance < Epsilon)
+            {
+                Vector2 beforeStep = _body.Position;
+                Debug.Log($"RemoveOverlap({collider.name}).substep#{MaxOverlapIterations - iteration} : " +
+                          $"remaining={separation.distance}, direction={separation.normal}");
+                _body.Position += separation.distance * separation.normal;
+
+                Vector2 afterStep = _body.Position;
+                Debug.DrawLine(beforeStep, afterStep, Color.yellow, 1f);
+            }
+            Vector2 endPosition = _body.Position;
+
+            // bias the resolved position ever so slightly along the normal to prevent contact
+            _body.Position += Epsilon * (endPosition - startPosition).normalized;
+        }
+
+
+        /* Project AABB along delta until (if any) obstruction. Max distance caps at body-radius to prevent tunneling. */
+        public void Move(Vector2 delta)
+        {
+            // note that we compare extremely close to zero rather than our larger epsilon,
+            // as delta can be very small depending on the physics step duration used to compute it
+            if (delta == Vector2.zero)
+            {
+                return;
+            }
+
+            Vector2 startPosition = _body.Position;
+            int iteration = MaxMoveIterations;
+            float distanceRemaining = delta.magnitude;
+            Vector2 direction = delta.normalized;
+            while (iteration-- > 0 && distanceRemaining > Epsilon && direction.sqrMagnitude > Epsilon)
+            {
+                Vector2 beforeStep = _body.Position;
+                Debug.DrawLine(beforeStep, beforeStep + (distanceRemaining * direction), Color.gray, 1f);
+                
+                Debug.Log($"Move({delta}).substep#{MaxMoveIterations-iteration} : " +
+                          $"remaining={distanceRemaining}, direction={direction}");
+                MoveUnobstructed(
+                    direction,
+                    distanceRemaining,
+                    out float step,
+                    out RaycastHit2D obstruction);
+
+                Vector2 afterStep = _body.Position;
+                Debug.DrawLine(beforeStep, afterStep, Color.green, 1f);
+
+                direction -= obstruction.normal * Vector2.Dot(direction, obstruction.normal);
+                distanceRemaining -= step;
+            }
+            Vector2 endPosition = _body.Position;
+            _body.MovePositionWithoutBreakingInterpolation(startPosition, endPosition);
+        }
+
+
+        /* Project body along delta until (if any) obstruction. Distance swept is capped at body-radius to prevent tunneling. */
+        private void MoveUnobstructed(Vector2 direction, float distance, out float step, out RaycastHit2D obstruction)
+        {
+            float bodyRadius = _body.ComputeDistanceToEdge(direction);
+
+            step = distance < bodyRadius ? distance : bodyRadius;
+            if (_body.CastAABB(direction, step + ContactOffset, out var hit))
+            {
+                float distancePastOffset = hit.distance - ContactOffset;
+                step = distancePastOffset < Epsilon? 0f : distancePastOffset;
+                obstruction = hit;
+            }
+            else
+            {
+                obstruction = default;
+            }
+
+            _body.Position += step * direction;
+        }
+    }
+}
