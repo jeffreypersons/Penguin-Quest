ab469a860135292bad86ebd6860d34e8851ad324
Jeffrey Persons <jperman8@gmail.com>
2023-07-29 10:38:54 -0400

    Initial duplication of contact check scene

---
+++ Assets/_Experimental/Sandbox_Physics/Contact_002__CancelContacts/Body.cs
@@ -0,0 +1,109 @@
+using System;
+using UnityEngine;
+
+
+namespace PQ._Experimental.Physics.Contact_002
+{
+    [Flags]
+    public enum CollisionFlags2D
+    {
+        None   = 0,
+        Front  = 1 << 1,
+        Below  = 1 << 2,
+        Behind = 1 << 3,
+        Above  = 1 << 4,
+        All    = ~0,
+    }
+    internal sealed class Body
+    {
+        private Transform         _transform;
+        private Rigidbody2D       _rigidbody;
+        private CapsuleCollider2D _capsuleCollider;
+        private ContactFilter2D   _contactFilter;
+        private ContactPoint2D[]  _contactBuffer;
+
+        public Vector2 Position => _rigidbody.position;
+        public Vector2 Extents  => _capsuleCollider.bounds.extents;
+        public Vector2 Forward  => _transform.right.normalized;
+        public Vector2 Up       => _transform.up.normalized;
+
+        
+        public bool IsFlippedHorizontal => _rigidbody.transform.localEulerAngles.y >= 90f;
+        public bool IsFlippedVertical   => _rigidbody.transform.localEulerAngles.x >= 90f;
+
+        public Body(Transform transform)
+        {
+            if (transform == null)
+            {
+                throw new ArgumentNullException(nameof(transform));
+            }
+            if (!transform.TryGetComponent(out Rigidbody2D rigidbody2D))
+            {
+                throw new MissingComponentException($"Expected attached {nameof(Rigidbody2D)} - not found on {nameof(transform)}");
+            }
+            if (!transform.TryGetComponent(out CapsuleCollider2D capsuleCollider2D))
+            {
+                throw new MissingComponentException($"Expected attached {nameof(BoxCollider2D)} - not found on {nameof(transform)}");
+            }
+            if (!ReferenceEquals(capsuleCollider2D.attachedRigidbody, rigidbody2D))
+            {
+                throw new MissingComponentException($"Expected attached {nameof(Rigidbody2D)} - not found on {nameof(capsuleCollider2D)}");
+            }
+
+            _transform       = rigidbody2D.transform;
+            _rigidbody       = rigidbody2D;
+            _capsuleCollider = capsuleCollider2D;
+            _contactFilter   = new ContactFilter2D();
+            _contactBuffer   = new ContactPoint2D[16];
+
+            _contactFilter.useTriggers    = false;
+            _contactFilter.useNormalAngle = false;
+            _contactFilter.SetLayerMask(LayerMask.GetMask("Solids"));
+
+            _rigidbody.simulated   = true;
+            _rigidbody.isKinematic = true;
+            _rigidbody.useFullKinematicContacts = true;
+            _rigidbody.constraints = RigidbodyConstraints2D.None;
+        }
+
+
+        public CollisionFlags2D CheckSides()
+        {
+            _contactFilter.useNormalAngle = true;
+            bool isFlippedHorizontal = _rigidbody.transform.localEulerAngles.y >= 90f;
+            bool isFlippedVertical   = _rigidbody.transform.localEulerAngles.x >= 90f;
+
+            CollisionFlags2D flags = CollisionFlags2D.None;
+            if (HasContactsInNormalRange(315, 45))
+            {
+                flags |= isFlippedHorizontal ? CollisionFlags2D.Behind : CollisionFlags2D.Front;
+            }
+            if (HasContactsInNormalRange(45, 135))
+            {
+                flags |= isFlippedVertical ? CollisionFlags2D.Above : CollisionFlags2D.Below;
+            }
+            if (HasContactsInNormalRange(135, 225))
+            {
+                flags |= isFlippedHorizontal ? CollisionFlags2D.Front : CollisionFlags2D.Behind;
+            }
+            if (HasContactsInNormalRange(225, 315))
+            {
+                flags |= isFlippedVertical ? CollisionFlags2D.Below : CollisionFlags2D.Above;
+            }
+            _contactFilter.useNormalAngle = false;
+            return flags;
+        }
+
+        private bool HasContactsInNormalRange(float min, float max)
+        {
+            float previousMin = _contactFilter.minNormalAngle;
+            float previousMax = _contactFilter.maxNormalAngle;
+
+            _contactFilter.SetNormalAngle(min, max);
+            bool hasContactsInRange = _capsuleCollider.IsTouching(_contactFilter);
+
+            _contactFilter.SetNormalAngle(previousMin, previousMax);
+            return hasContactsInRange;
+        }
+    }
+}
