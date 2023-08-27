e384ecd762ec774de10fa1428c8b2b686e9e207b
Jeffrey Persons <jperman8@gmail.com>
2023-08-27 16:09:55 -0400

    Fix debug tag

--- Assets/_Experimental/Sandbox_Physics/Move_006__SurfaceSlidingConcaveHandling/KinematicLinearSolver2D.cs
+++ Assets/_Experimental/Sandbox_Physics/Move_006__SurfaceSlidingConcaveHandling/KinematicLinearSolver2D.cs
@@ -56,7 +56,7 @@ namespace PQ._Experimental.Physics.Move_006
             {
                 // any surface passed through (tunneling) for the initial separation can be prevented by moving back along normal
                 // May occur when body is heavily overlapped with an edge collider causing it 'snap' to other side
-                Debug.Log($"RemoveOverlap({collider.name}).substep#precheck : Initial resolution caused collider to pass through an edge - pushing back to compensate");
+                Debug.Log($"ResolveSeparation({collider.name}).substep#precheck : Initial resolution caused collider to pass through an edge - pushing back to compensate");
                 _body.IntersectAABB(_body.Position, separation.normal, out float distanceToAABBEdge);
                 _body.Position += -2f * distanceToAABBEdge * separation.normal;
             }
@@ -69,12 +69,12 @@ namespace PQ._Experimental.Physics.Move_006
                 Vector2 beforeStep = _body.Position;
                 separation = _body.ComputeMinimumSeparation(collider);
 
-                Debug.Log($"RemoveOverlap({collider.name}).substep#{MaxOverlapIterations - iteration} : remaining={separation.distance}, direction={separation.normal}");
+                Debug.Log($"ResolveSeparation({collider.name}).substep#{MaxOverlapIterations - iteration} : remaining={separation.distance}, direction={separation.normal}");
 
                 if (Mathf.Abs(separation.distance) > Mathf.Abs(previousSeparation.distance))
                 {
                     // Any time separation not decreasing then bail out. May occur when moving into a protruding corner causing over-correction.
-                    Debug.Log($"RemoveOverlap({collider.name}) : Separation amount increased from {previousSeparation.distance} to {separation.distance} - halting resolution");
+                    Debug.Log($"ResolveSeparation({collider.name}) : Separation amount increased from {previousSeparation.distance} to {separation.distance} - halting resolution");
                     break;
                 }
                 _body.Position += separation.distance * separation.normal;
