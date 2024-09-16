e835e665d6d2e44f8e40558fb61ce386ebd85805
Jeffrey Persons <jperman8@gmail.com>
2024-09-16 09:46:05 -0400

    Restructure kindmatic body

--- Assets/_Experimental/Sandbox_Physics/Move_007__SurfaceSlidingEnsuringContactOffsets/KinematicBody2D.cs
+++ Assets/_Experimental/Sandbox_Physics/Move_007__SurfaceSlidingEnsuringContactOffsets/KinematicBody2D.cs
@@ -4,34 +4,40 @@ using UnityEngine;
 
 namespace PQ._Experimental.Physics.Move_007
 {
-    [Flags]
-    public enum ContactSlotId
-    {
-        None              = 0,
-        LeftSide          = 1 << 1,
-        BottomLeftCorner  = 1 << 2,
-        BottomSide        = 1 << 3,
-        BottomRightCorner = 1 << 4,
-        RightSide         = 1 << 5,
-        TopRightCorner    = 1 << 6,
-        TopSide           = 1 << 7,
-        TopLeftCorner     = 1 << 8,
-        All               = ~0,
-    }
-
-    public struct ContactSlot
-    {
-        public ContactSlotId Id           { get; init; }
-        public Vector2       Normal       { get; init; }
-        public Vector2       ScanOrigin   { get; set;  }
-        public float         ScanDistance { get; set;  }
-        public RaycastHit2D  ScanHit      { get; set;  }
-
-        public override string ToString() => $"{Id}: {(ScanHit ? ScanHit.distance : "-")}";
-    }
-
     internal sealed class KinematicBody2D
     {
+        public enum ContactSlotId
+        {
+            RightSide,
+            TopRightCorner,
+            TopSide,
+            TopLeftCorner,
+            LeftSide,
+            BottomLeftCorner,
+            BottomSide,
+            BottomRightCorner,
+        }
+
+        public struct ContactSlot<SlotId> where SlotId : struct, Enum
+        {
+            public SlotId        Id           { get;      }
+            public Vector2       Anchor       { get;      }
+            public Vector2       Normal       { get;      }
+            public Vector2       ScanOrigin   { get; set; }
+            public float         ScanDistance { get; set; }
+            public RaycastHit2D  ScanHit      { get; set; }
+
+            public ContactSlot(SlotId id, Vector2 anchor)
+            {
+                Id           = id;
+                Anchor       = anchor;
+                Normal       = anchor.normalized;
+                ScanOrigin   = default;
+                ScanDistance = default;
+                ScanHit      = default;
+            }
+        }
+
         private Transform        _transform;
         private Rigidbody2D      _rigidbody;
         private BoxCollider2D    _boxCollider;
@@ -41,18 +47,11 @@ namespace PQ._Experimental.Physics.Move_007
         private RaycastHit2D[]   _hitBuffer;
         private Collider2D[]     _overlapBuffer;
         private ContactPoint2D[] _contactBuffer;
-        private ContactSlot[]    _slots;
+        private ContactSlot<ContactSlotId>[] _slots;
 
         private LayerMask _previousLayerMask;
 
-        private const float DefaultEpsilon = 0.005f;
         private const int DefaultBufferSize = 6;
-        private static readonly Vector2 NormalizedDiagonal = Vector2.one.normalized;
-
-        private static readonly Vector2[] SlotAnchors = new Vector2[]
-        {
-            new(1,0), new(1,1), new(0,1), new(-1,1), new(-1,0), new(-1,-1), new(0,-1), new(1,-1),
-        };
 
         public override string ToString() =>
             $"{GetType()}{{" +
@@ -82,7 +81,7 @@ namespace PQ._Experimental.Physics.Move_007
         public Vector2 Center    => _boxCollider.bounds.center;
         public Vector2 Forward   => _transform.right.normalized;
         public Vector2 Up        => _transform.up.normalized;
-        public Vector2 Extents   => _boxCollider.bounds.extents + new Vector3(_boxCollider.edgeRadius, _boxCollider.edgeRadius, 0f);
+        public Vector2 Extents   => _boxCollider.bounds.extents;
         public float   Depth     => _transform.position.z;
         public float   SkinWidth => _boxCollider.edgeRadius;
 
@@ -151,21 +150,17 @@ namespace PQ._Experimental.Physics.Move_007
             _rigidbody.isKinematic = true;
             _rigidbody.useFullKinematicContacts = true;
             _rigidbody.constraints = RigidbodyConstraints2D.None;
-
-            bool isDiagonal = false;
-            _slots = new ContactSlot[SlotAnchors.Length];
-            for (int index = 0; index < SlotAnchors.Length; index++)
+            _slots = new ContactSlot<ContactSlotId>[]
             {
-                _slots[index] = new ContactSlot
-                {
-                    Id           = (ContactSlotId)index,
-                    Normal       = isDiagonal ? SlotAnchors[index] * NormalizedDiagonal : SlotAnchors[index],
-                    ScanOrigin   = default,
-                    ScanDistance = default,
-                    ScanHit      = default,
-                };
-                isDiagonal = !isDiagonal;
-            }
+                new(ContactSlotId.RightSide,         new Vector2( 1,  0)),
+                new(ContactSlotId.TopRightCorner,    new Vector2( 1,  1)),
+                new(ContactSlotId.TopSide,           new Vector2( 0,  1)),
+                new(ContactSlotId.TopLeftCorner,     new Vector2(-1,  1)),
+                new(ContactSlotId.LeftSide,          new Vector2(-1,  0)),
+                new(ContactSlotId.BottomLeftCorner,  new Vector2(-1, -1)),
+                new(ContactSlotId.BottomSide,        new Vector2( 0, -1)),
+                new(ContactSlotId.BottomRightCorner, new Vector2( 1, -1)),
+            };
         }
 
         /* Check if body is filtering out collisions with given object or not. */
@@ -207,47 +202,6 @@ namespace PQ._Experimental.Physics.Move_007
             _rigidbody.MovePosition(targetPositionThisFrame);
         }
         
-        
-        /*
-        Check if body center is fully surrounded by the same edge collider.
-        
-        Considered to be 'inside' if there is an edge collider above center of our AABB, and the same edge collider below.
-        Assumes there aren't any edge collider inside another.
-        */
-        public bool IsCenterBoundedByAnEdgeCollider(out EdgeCollider2D collider)
-        {
-            Vector2 origin = _boxCollider.bounds.center;
-
-            if (!CastRay(origin, Vector2.up, Mathf.Infinity, out var aboveHit) ||
-                !aboveHit.collider.transform.TryGetComponent<EdgeCollider2D>(out var edge))
-            {
-                collider = default;
-                return false;
-            }
-
-            float maxHorizontal = 2f * edge.bounds.extents.x;
-            float maxVertical   = 2f * edge.bounds.extents.y;
-            if (!CastRayAt(edge, origin, Vector2.down,  maxVertical,   out var _) ||
-                !CastRayAt(edge, origin, Vector2.left,  maxHorizontal, out var _) ||
-                !CastRayAt(edge, origin, Vector2.right, maxHorizontal, out var _))
-            {
-                collider = default;
-                return false;
-            }
-
-            collider = edge;
-            return true;
-        }
-        
-        /* Check if given world point lies on or within our AABB. */
-        public bool IsPointInBounds(Vector2 point)
-        {
-            Bounds bounds = _boxCollider.bounds;
-            bounds.Expand(_boxCollider.edgeRadius);
-            return bounds.Contains(point);
-        }
-
-
         /* Expand out bounds from body center, ignoring attached AABB, outputting all overlapping colliders. Note order is not significant. */
         public bool CheckForOverlappingColliders(Vector2 extents, out ReadOnlySpan<Collider2D> colliders)
         {
@@ -267,10 +221,7 @@ namespace PQ._Experimental.Physics.Move_007
         */
         public bool IntersectAABB(Vector2 origin, Vector2 direction, out float distanceToEdge)
         {
-            Bounds bounds = _boxCollider.bounds;
-            bounds.Expand(_boxCollider.edgeRadius);
-
-            bool foundIntersection = bounds.IntersectRay(new Ray(origin, direction), out distanceToEdge);
+            bool foundIntersection = _boxCollider.bounds.IntersectRay(new Ray(origin, direction), out distanceToEdge);
 
             // discard sign since distance is negative if starts within bounds (contrary to other ray methods)
             distanceToEdge = Mathf.Abs(distanceToEdge);
@@ -281,13 +232,17 @@ namespace PQ._Experimental.Physics.Move_007
         }
 
         /* Starting from right going counter-clockwise, sweeptest given distance out from AABB. */
-        public void FireAllContactSensors(float contactOffset, out ReadOnlySpan<ContactSlot> slots)
+        public void FireAllContactSensors(float distance, out ReadOnlySpan<ContactSlot<ContactSlotId>> slots)
         {
-            Vector2 center = _boxCollider.bounds.center;
-            Vector2 extents = (Vector2)_boxCollider.bounds.extents;
+            // todo: replace full sweeptest with BoxCast(origin: slot.position)
+            // todo: create and use a higher level contact struct for above/below/behind/front
+            // Note that for each slot, scale anchor [point on edge of a unit square] by extents
+            // For example, for xy extents (1 / 4, 1) the scale is (0.50, 2)
+            Vector2 center  = _boxCollider.bounds.center;
+            Vector2 extents = _boxCollider.bounds.extents;
             for (int index = 0; index < _slots.Length; index++)
             {
-                Scan(ref _slots[index], center, extents, contactOffset);
+                FireContactSensor(ref _slots[index], center, extents, distance);
             }
             slots = _slots.AsSpan();
         }
@@ -318,22 +273,6 @@ namespace PQ._Experimental.Physics.Move_007
             else
             {
                 hit = default;
-            }
-            ReEnableCollisionsWithAABB();
-            #if UNITY_EDITOR
-            DrawCastInEditorIfEnabled(origin, direction, distance, hit? hit.distance : null);
-            #endif
-            return hit;
-        }
-
-        /* Project a circle along delta, ignoring attached AABB, and stopping at first hit (if any). */
-        public bool CastCircle(Vector2 origin, float radius, Vector2 direction, float distance, out RaycastHit2D hit)
-        {
-            DisableCollisionsWithAABB();
-            hit = default;
-            if (Physics2D.CircleCast(origin, radius, direction, _contactFilter, _singleHit, distance) > 0)
-            {
-                hit = _singleHit[0];
             }
             ReEnableCollisionsWithAABB();
             #if UNITY_EDITOR
@@ -429,6 +368,7 @@ namespace PQ._Experimental.Physics.Move_007
         public bool CastRaysFromCorner(float spreadExtent, Vector2 direction, float distance, int rayCount, out int hitCount, out ReadOnlySpan<RaycastHit2D> results)
         {
             (Vector2 normal, Vector2 position) = FindClosestCorner(direction);
+
             Vector2 tangent = Vector2.Perpendicular(normal);
             Vector2 start   = position + spreadExtent * tangent;
             Vector2 end     = position - spreadExtent * tangent;
@@ -470,10 +410,11 @@ namespace PQ._Experimental.Physics.Move_007
                 _       => new Vector2(-1, -1),
             };
 
+            Vector2 normalizedDiagonal = Vector2.one.normalized;
             Vector2 center       = _boxCollider.bounds.center;
             Vector2 extents      = _boxCollider.bounds.extents;
-            Vector2 radialOffset = _boxCollider.edgeRadius * NormalizedDiagonal;
-            return (sign * NormalizedDiagonal, center + sign * (extents + radialOffset));
+            Vector2 radialOffset = _boxCollider.edgeRadius * normalizedDiagonal;
+            return (sign * normalizedDiagonal, center + sign * (extents + radialOffset));
         }
 
         /*
@@ -487,7 +428,7 @@ namespace PQ._Experimental.Physics.Move_007
             float degrees = Vector2.SignedAngle(new Vector2(1, -1), direction);
             if (degrees <= 0)
             {
-                degrees = 360f + degrees;
+                degrees += 360f;
             }
             (Vector2 normal, Vector2 cornerStart, Vector2 cornerEnd) = degrees switch
             {
@@ -498,15 +439,16 @@ namespace PQ._Experimental.Physics.Move_007
             };
             
             Vector2 center  = _boxCollider.bounds.center;
-            Vector2 extents = (Vector2)_boxCollider.bounds.extents + new Vector2(_boxCollider.edgeRadius, _boxCollider.edgeRadius);
+            Vector2 extents = (Vector2)_boxCollider.bounds.extents;
             return (normal, center + extents * cornerStart, center + extents * cornerEnd);
         }
 
-        private void Scan(ref ContactSlot slot, Vector2 center, Vector2 extents, float distance)
+        private void FireContactSensor(ref ContactSlot<ContactSlotId> slot, Vector2 center, Vector2 extents, float distance)
         {
+            // todo: replace full sweeptest with BoxCast(origin: slot.position)
             // Scale anchor [point on edge of a unit square] by extents.
             // For example, for xy extents (1 / 4, 1) the scale is (0.50, 2)
-            Vector2 offset = Vector2.Scale(SlotAnchors[(int)slot.Id], extents);
+            Vector2 offset = Vector2.Scale(slot.Anchor, extents);
 
             slot.ScanOrigin = center + offset;
             slot.ScanDistance = (new Vector2(distance, distance) * slot.Normal).magnitude;
@@ -518,9 +460,7 @@ namespace PQ._Experimental.Physics.Move_007
             {
                 slot.ScanHit = default;
             }
-
             #if UNITY_EDITOR
-            Debug.Log($"{slot.Id} : from={slot.ScanOrigin} to={slot.ScanOrigin + slot.ScanDistance * slot.Normal}");
             DebugExtensions.DrawRayCast(slot.ScanOrigin, slot.Normal, slot.ScanDistance, slot.ScanHit, Time.fixedDeltaTime);
             #endif
         }
