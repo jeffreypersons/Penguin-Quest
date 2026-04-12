221561e5fb215f472909dbc24994b4200db79cda
Jeffrey Persons <jperman8@gmail.com>
2026-04-12 18:20:08 -0400

    Fix triggers not working and hook up start/end scenes

--- Assets/Code/Game/Interaction/LevelEndTrigger.cs
+++ Assets/Code/Game/Interaction/LevelEndTrigger.cs
@@ -7,13 +7,37 @@ namespace PQ.Game.Interaction
     public class LevelEndTrigger : MonoBehaviour
     {
         private bool _triggered;
+        private BoxCollider2D _collider;
+        private ContactFilter2D _filter;
+        private Collider2D[] _results;
 
-        private void OnTriggerEnter2D(Collider2D collision)
+        private void Awake()
         {
-            if (!_triggered && collision.CompareTag("Player"))
+            gameObject.layer = LayerMask.NameToLayer("Triggers");
+            _collider = GetComponent<BoxCollider2D>();
+            _results = new Collider2D[4];
+            _filter = new ContactFilter2D();
+            _filter.useTriggers = false;
+            _filter.SetLayerMask(LayerMask.GetMask("Actors"));
+        }
+
+        private void FixedUpdate()
+        {
+            if (_triggered)
             {
-                _triggered = true;
-                GameEventCenter.Instance.endGame.Raise();
+                return;
+            }
+
+            int count = _collider.Overlap(_filter, _results);
+            for (int i = 0; i < count; i++)
+            {
+                if (_results[i].CompareTag("Player"))
+                {
+                    _triggered = true;
+                    Debug.Log("LevelEndTrigger: Player entered, raising endGame");
+                    GameEventCenter.Instance.endGame.Raise();
+                    return;
+                }
             }
         }
     }
